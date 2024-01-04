c8628ee7138197cf42fe4f0db9812a3bcbced0f7
Vikash <vikash@shauryasoft.com>
2024-01-04 16:31:19 +0530

    table updated

---
+++ src/views/sidebar-menus/timetable/Timetablelist.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Grid, Paper, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+
+function createData(key, sub, time) {
+  return { key, sub, time };
+}
+
+const rows = [
+  createData(1, 'English', '8:15am to 9:15am'),
+  createData(2, 'Mathematics', '9:15am to 10:15am'),
+  createData(3, 'Science', '10:15am to 11:00am'),
+  createData('', 'Brack', '11:00am to 11:15am'),
+  createData(4, 'Hindi', '11:15am to 12:00am'),
+  createData(5, 'Physics', '12:00am to 12:45pm'),
+  createData('', 'Lunch Brack', '12:45am to 01:45pm'),
+  createData(6, 'Chemistry', '12:45am to 01:45pm'),
+  createData(7, 'Computer', '12:45am to 01:45pm'),
+  // Add more rows as needed
+];
+
+export default function BasicList() {
+  return (
+
+    <Grid container spacing={2}>
+      {rows.map((row) => (
+        <Grid item xs={12} key={row.key}>
+          <Paper elevation={3} style={{ padding: '15px', textAlign: 'center', display: 'flex',lineHeight: '10px' }}>
+              <Typography variant="h6" sx={{ background:'#ede7f6' }}>
+              <Box sx={{ m: 2 }}>{row.key}</Box>
+              </Typography>
+              <Typography variant="body1">
+              <Box sx={{ m: 2 }}>{row.sub}</Box>
+              </Typography>
+              <Typography variant="body1" sx={{ marginLeft: 'auto' }}>
+              <Box sx={{ m: 2 }}>{row.time}</Box>
+              </Typography>
+          </Paper>
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
