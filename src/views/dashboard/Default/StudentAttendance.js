b3300b91e7602d2b322fb1e8b77550d1d40d973e
Abhishek <abhishek@shauryasoft.com>
2024-01-06 12:40:10 +0530

    profile

---
+++ src/views/dashboard/Default/StudentAttendance.js
@@ -0,0 +1,6 @@
+const attendanceData = [
+    { date: '2022-01-01', present: 20, absent: 5 },
+    { date: '2022-01-02', present: 18, absent: 7 },
+    // Add more data as needed
+  ];
+  
--- src/views/dashboard/Profile.js
+++ src/views/dashboard/Profile.js
@@ -1,28 +1,24 @@
-import React from 'react'
-import {Avatar , Box, Typography , Button} from '@mui/material';
-import User1 from 'assets/images/users/user-round.svg';
+import React from 'react';
+import { Avatar, Box, Typography, Button } from '@mui/material';
+import { studentProfileDetails } from 'layout/MainLayout/Header/ProfileSection/ProfileDeatails';
+import { useNavigate } from 'react-router-dom'; 
 
-const Profile = () => {
-  const StudentName = 'Suraj'
-  const AdminNo = 'V-A (Adm No : D00158)'
+
+  const Profile = () => {
+    const navigate = useNavigate(); 
+  
   return (
-    <> 
-    {/* <Avatar src={User1}/> */}
- 
-
-    <Box sx={{textAlign:'center', marginTop:'1rem' , border:'1px solid rgb(185 185 185)', padding:'2rem'}}>
-    <Avatar alt="Remy Sharp" src={User1} sx={{ width: 200, height: 200 , margin:'auto', marginBottom : '10px' }}/>
-      <Typography variant='h2'>{StudentName}</Typography> 
-      <Typography variant='h5'>{AdminNo}</Typography>
-      <Button sx={{textTransform: 'uppercase' , background:'rgb(33, 150, 243)'}} variant="Profile" >Profile</Button>
-      
-    </Box>
-
+    <>
+      <Box sx={{ textAlign: 'center',  border: '1px solid rgb(185 185 185)',borderRadius:'7px', padding: '2rem' }}>
+        <Avatar alt="Remy Sharp" src={studentProfileDetails.StudentImage} sx={{ width: 150, height: 150, margin: 'auto', marginBottom: '10px' }} />
+        <Typography variant="h2">{studentProfileDetails.StudentName}</Typography>
+        <Typography variant="h5">{studentProfileDetails.AdminNo}</Typography>
+        <Button sx={{ textTransform: 'uppercase', background: 'rgb(33, 150, 243)', color: '#fff' , marginTop:'1rem' }} variant="contained"  onClick={() => navigate('/student-profile')}>
+          go to Profile
+        </Button>
+      </Box>
     </>
+  );
+};
 
-
-  )
-}
-
-export default Profile
-
+export default Profile;
