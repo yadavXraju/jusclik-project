8c5608954f12bb3b7827de3aa0f4bf5954803155
Ruby <ruby@shauryasoft.com>
2024-01-06 12:52:17 +0530

    done

--- src/views/dashboard/Default/TotalGrowthBarChart.js
+++ src/views/dashboard/Default/TotalGrowthBarChart.js
@@ -100,10 +100,10 @@ const TotalGrowthBarChart = ({ isLoading }) => {
                 <Grid item>
                   <Grid container direction="column" spacing={1}>
                     <Grid item>
-                      <Typography variant="subtitle2">Total Growth</Typography>
+                      <Typography variant="subtitle2">Total Attendance</Typography>
                     </Grid>
                     <Grid item>
-                      <Typography variant="h3">$2,324.00</Typography>
+                      <Typography variant="h3">26 Days</Typography>
                     </Grid>
                   </Grid>
                 </Grid>
--- src/views/sidebar-menus/attendance/index.js
+++ src/views/sidebar-menus/attendance/index.js
@@ -3,19 +3,28 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 import ProgressAttendance from './ProgressAttendance';
-import AttendanceChart from './AttendanceChart'
+import AttendanceChart from './AttendanceChart';
 import Leaves from './Leaves';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
 export default function BasicDateCalendar() {
   return (
     <>
-    <ProgressAttendance/>
-    <LocalizationProvider dateAdapter={AdapterDayjs} >
-      <DateCalendar />
-    </LocalizationProvider>
-    <AttendanceChart/>
-    <Leaves/>
-    
+      <ProgressAttendance />
+      <Box>
+        <Grid container spacing={2} justifyContent="center" alignItems="center">
+          <Grid item xs={12} sm={6} md={4}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DateCalendar />
+            </LocalizationProvider>
+          </Grid>
+          <Grid item xs={12} sm={6} md={8}>
+            <AttendanceChart />
+          </Grid>
+        </Grid>
+      </Box>
+      <Leaves />
     </>
   );
 }
