3f7be52d3d5fb8b86707f8b6ba781fe91739b2ba
Ruby <ruby@shauryasoft.com>
2024-01-06 09:38:43 +0530

    d

--- src/views/sidebar-menus/assignments/index.js
+++ src/views/sidebar-menus/assignments/index.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserSecret, faCoffee, faMessage, faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Assignments = () => {
   return (
-    <div>Assignments</div>
-  )
+    <>
+      <FontAwesomeIcon icon={faUserSecret} />
+      <FontAwesomeIcon icon={faCoffee} />
+      <FontAwesomeIcon icon={faMessage} />
+      <FontAwesomeIcon icon={faUser} />
+    </>
+  );
 }
 
-export default Assignments
+export default Assignments;
