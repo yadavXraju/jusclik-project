6dcf045a0b60fb4c45e8bb89f26b36a0f45d21a9
Ruby <ruby@shauryasoft.com>
2024-01-13 09:48:27 +0530

    login

--- src/views/sidebar-menus/attendance/AttendanceChart.js
+++ src/views/sidebar-menus/attendance/AttendanceChart.js
@@ -1,12 +1,10 @@
 import React from 'react'
-// import TotalGrowthBarChart from '../../dashboard/Default/TotalGrowthBarChart';
 import AttendanceGraph from './AttendaceGraph'
 import TotalGrowthBarChart from './TotalAttendanceBar'
 
 const AttendanceChart = () => {
   return (
     <>
-      {/* <TotalGrowthBarChart/> */}
       <AttendanceChart/>
       <TotalGrowthBarChart/>
     </>
