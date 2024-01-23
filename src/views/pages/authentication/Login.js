2ac83414b260ffac942a7c49dcf63db57aee6f13
Dinesh <tech@shauryasoft.com>
2024-01-23 14:44:37 +0530

    Done 23-jan-24

--- src/views/pages/authentication/Login.js
+++ src/views/pages/authentication/Login.js
@@ -25,242 +25,278 @@ import PlayStoreImage from '../../../assets/images/Google-Play.png';
 import AppleStoreImage from '../../../assets/images/app-store.png';
 import CloudLogo from '../../../assets/images/Untitled-2.png';
 import { useNavigate } from 'react-router-dom';
-import EnterMobileDialog from './EnterMobileDialog'
+import EnterMobileDialog from './EnterMobileDialog';
+import axios from 'axios';
+import SetSession from '../../../session/setsession';
+import Config from '../../../config';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import UserProfile from "../../../session/userproile";
 
 const defaultTheme = createTheme();
 
 function ToggleGroupVariants() {
-  const navigate = useNavigate();
-  const [value, setValue] = React.useState('plain');
+const navigate = useNavigate();
+const [value, setValue] = React.useState('plain');
 
-  return (
-    <FormControl>
-      <Grid container>
-        <Grid item>
-          <ToggleButtonGroup
-            value={value}
-            exclusive
-            onChange={(event, newValue) => {
-              setValue(newValue);
-              setSelectedVariant(newValue);
-            }}
+return (
+  <FormControl>
+    <Grid container>
+      <Grid item>
+        <ToggleButtonGroup
+          value={value}
+          exclusive
+          onChange={(event, newValue) => {
+            setValue(newValue);
+            setSelectedVariant(newValue);
+          }}
+          style={{
+            borderRadius: '50px',
+            border: '2px solid #fff',
+          }}
+        >
+          <ToggleButton
             style={{
-              borderRadius: '50px',
-              border: '2px solid #fff',
+              borderRadius: '50px 0px 0px 50px',
+              width: '110px',
+              color: value === 'plain' ? '#000' : '#fff',
+              backgroundColor: value === 'plain' ? '#fff' : 'initial',
+              border: 'none', // Remove border for individual button
             }}
+            value="plain"
           >
-            <ToggleButton
-              style={{
-                borderRadius: '50px 0px 0px 50px',
-                width: '110px',
-                color: value === 'plain' ? '#000' : '#fff',
-                backgroundColor: value === 'plain' ? '#fff' : 'initial',
-                border: 'none', // Remove border for individual button
-              }}
-              value="plain"
-            >
-              Password
-            </ToggleButton>
-            <ToggleButton
-              onClick={() => navigate('/otplogin')}
-              style={{
-                borderRadius: '0px 50px 50px 0px',
-                width: '110px',
-                color: value === 'outlined' ? '#000' : '#fff',
-                backgroundColor: value === 'outlined' ? '#fff' : 'initial',
-                border: 'none', // Remove border for individual button
-              }}
-              value="outlined"
-            >
-              OTP
-            </ToggleButton>
-          </ToggleButtonGroup>
-        </Grid>
+            Password
+          </ToggleButton>
+          <ToggleButton
+            onClick={() => navigate('/otplogin')}
+            style={{
+              borderRadius: '0px 50px 50px 0px',
+              width: '110px',
+              color: value === 'outlined' ? '#000' : '#fff',
+              backgroundColor: value === 'outlined' ? '#fff' : 'initial',
+              border: 'none', // Remove border for individual button
+            }}
+            value="outlined"
+          >
+            OTP
+          </ToggleButton>
+        </ToggleButtonGroup>
       </Grid>
-    </FormControl>
-  );
+    </Grid>
+  </FormControl>
+);
 }
 
 export default function LogInPage() {
-  const [showPassword, setShowPassword] = useState(false);
+const [showPassword, setShowPassword] = useState(false);
 
-  const [dialogOpen, setDialogOpen] = useState(false);
+const [dialogOpen, setDialogOpen] = useState(false);
 
-  const handleDialogToggle = () => {
-    setDialogOpen(!dialogOpen);
-  };
+const handleDialogToggle = () => {
+  setDialogOpen(!dialogOpen);
+};
 
-  const handleClickOpen = () => {
-    setDialogOpen(true);
-  };
+const handleClickOpen = () => {
+  setDialogOpen(true);
+};
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('Username'),
-      password: data.get('password'),
-      otp: data.get('otp'),
-    });
-  };
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <Grid container component="main" sx={{ minHeight: '100vh' }}>
-        <CssBaseline />
-        {/* left school logo start */}
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={4}
-          sx={{
-            backgroundImage: `url(${Frame})`,
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'bottom',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <Grid item sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
-            <img src={LogoImg} alt="Logo" style={{ width: '200px', height: '200px' }} />
-            <Typography sx={{ fontWeight: '700', fontSize: { xs: '24px', md: '30px' }, color: '#364152b5' }}>
-              Arwachin Public School
-            </Typography>
-          </Grid>
-        </Grid>
-        {/* left school logo start */}
-        {/* right side background img */}
-        <Grid item xs={12} sm={8} md={8} component={Paper} elevation={6} square sx={{
-          backgroundImage: `url(${LoginImage} )`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'bottom', display: 'flex',
-          alignItems: 'center', justifyContent: 'center',
-        }}>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Typography component="h1" variant="h5" sx={{ color: 'white', fontWeight: '700', fontSize: '30px' }}>
-              LOGIN
-            </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-              {/* phone no. */}
-              <TextField
-                InputProps={{
-                  style: {
-                    borderRadius: '50px',
-                  },
-                }}
-                margin="normal"
-                required
-                fullWidth
-                id="Username"
-                label="Username"
-                name="Username"
-                autoComplete="Username"
-                autoFocus
-                sx={{ backgroundColor: '#fff', borderRadius: '50px' }}
-              />
-              <TextField
-                InputProps={{
-                  style: { borderRadius: '50px' },
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={() => setShowPassword(!showPassword)}
-                        edge="end"
-                        sx={{ color: '#364152b5', marginRight: '2px' }}
-                      >
-                        {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type={showPassword ? 'text' : 'password'}
-                id="password"
-                autoComplete="current-password"
-                sx={{ backgroundColor: '#fff', borderRadius: '50px' }}
-              />
+const handleSubmit = async (event) => {
+  event.preventDefault();
+  const data = new FormData(event.currentTarget);
+  await fetchData(data);
 
-              {/* Remember me */}
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" style={{ color: '#fff' }} />}
-                label={<Typography sx={{ color: '#fff' }}>Remember me</Typography>}
-              />
-              <Grid item xs sx={{ display: 'flex', justifyContent: 'end', alignItems: 'end', mt: -4 }} style={{ cursor: 'pointer' }}>
-                
-                <EnterMobileDialog open={dialogOpen} onClose={handleDialogToggle} onClick={handleClickOpen}/>
-                  {/* <Typography variant="body2" sx={{ cursor: 'pointer', color: '#fff' }} onClick={handleClickOpen}>
-                  Forgot Credentials
-                </Typography> */}
-              </Grid>
-              <Box sx={{ display: 'flex', alignItems: 'end', justifyContent: 'end', mt: 4 }}>
-                <ToggleGroupVariants />
-              </Box>
-              <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                <Button
-                  type="submit"
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2, backgroundColor: '#fff', color: '#364152b5', borderRadius: '50px', width: "150px", margin: '15px', display: 'flex', height: '45px' }}
-                >
-                  LOGIN
-                </Button>
-              </Box>
-              <Grid item sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', mt: 3 }}>
-                <img src={CloudLogo} alt="Logo" />
-                <Typography sx={{ color: '#fff', mt: 1 }}>
-                  Powered by
-                </Typography>
-                <Link href="http://www.shauryasoft.com" target="_blank" rel="noopener noreferrer" sx={{ color: '#fff', textDecoration: 'none' }}>
-                  www.shauryasoft.com
-                </Link>
-              </Grid>
-            </Box>
-            <Box sx={{ mt: 2, display: 'flex', width: '100%', justifyContent: 'center', gap: '20px' }}>
-              <Button
-                variant="contained"
-                color="primary"
-                href="PLAY_STORE_LINK"
-                target="_blank"
-                rel="noopener noreferrer"
-                size="small"
-                sx={{ height: '50px', width: "150px", display: 'flex', backgroundColor: '#fff', color: '#000', borderRadius: '10px', lineHeight: '15px' }}
-                startIcon={<img src={PlayStoreImage} alt="Play Store" style={{ width: '20px', marginRight: '10px' }} />}
-              >
-                GET IT ON Google Play
-              </Button>
-              <Button
-                variant="contained"
-                color="primary"
-                href="APPLE_STORE_LINK"
-                target="_blank"
-                rel="noopener noreferrer"
-                size="small"
-                sx={{ height: '50px', width: "150px", display: 'flex', backgroundColor: '#fff', color: '#000', borderRadius: '10px', lineHeight: '15px' }}
-                startIcon={<img src={AppleStoreImage} alt="Apple Store" style={{ width: '25px', marginRight: '10px' }} />}
-              >
-                GET IT ON Apple Store
-              </Button>
-            </Box>
-            <Typography variant='subtitle1' color='#fff' sx={{ mt: 1 }}>
-              Online Fee Payment Policies/ Terms & Conditions
-            </Typography>
-          </Box>
+  
+};
+const navigate = useNavigate(); 
+const fetchData = async (Data) => {
+  try {
+    const response = await axios.get(
+      Config.apiUrl + '/login',
+      { 
+        params: {
+          SCode: 6,
+          LoginID: Data.get('Username'),
+          Password: Data.get('password'),
+          DeviceType: 0,
+          DeviceID: '',
+          MobileApp: 0,
+        },
+      }
+    );
+    var jResponse = JSON.parse(response.data);
+    if (jResponse.authcode !== null && jResponse.authcode !== ''&& jResponse.authcode !== undefined ) {
+      localStorage.setItem("token",jResponse.authcode);
+      SetSession();
+      const userProfileData = UserProfile.getData();
+      console.log(userProfileData);
+      //navigate('/dashboard');
+    } else {
+     alert('Invaild Authentication');
+    }
+  } catch (error) {
+    console.log('Error fetching data:' + error);
+  }
+};
+
+//const navigate = useNavigate(); 
+
+return (
+  <ThemeProvider theme={defaultTheme}>
+    <Grid container component="main" sx={{ minHeight: '100vh' }}>
+      <CssBaseline />
+      {/* left school logo start */}
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={4}
+        sx={{
+          backgroundImage: `url(${Frame})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundColor: (t) =>
+            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+          backgroundSize: 'cover',
+          backgroundPosition: 'bottom',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Grid item sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+          <img src={LogoImg} alt="Logo" style={{ width: '200px', height: '200px' }} />
+          <Typography sx={{ fontWeight: '700', fontSize: { xs: '24px', md: '30px' }, color: '#364152b5' }}>
+            Arwachin Public School
+          </Typography>
         </Grid>
       </Grid>
-    </ThemeProvider>
-  );
+      {/* left school logo start */}
+      {/* right side background img */}
+      <Grid item xs={12} sm={8} md={8} component={Paper} elevation={6} square sx={{
+        backgroundImage: `url(${LoginImage} )`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'bottom', display: 'flex',
+        alignItems: 'center', justifyContent: 'center',
+      }}>
+        <Box
+          sx={{
+            my: 8,
+            mx: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h5" sx={{ color: 'white', fontWeight: '700', fontSize: '30px' }}>
+            LOGIN
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            {/* phone no. */}
+            <TextField
+              InputProps={{
+                style: {
+                  borderRadius: '50px',
+                },
+              }}
+              margin="normal"
+              required
+              fullWidth
+              id="Username"
+              label="Username"
+              name="Username"
+              autoComplete="Username"
+              autoFocus
+              sx={{ backgroundColor: '#fff', borderRadius: '50px' }}
+            />
+            <TextField
+              InputProps={{
+                style: { borderRadius: '50px' },
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={() => setShowPassword(!showPassword)}
+                      edge="end"
+                      sx={{ color: '#364152b5', marginRight: '2px' }}
+                    >
+                      {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type={showPassword ? 'text' : 'password'}
+              id="password"
+              autoComplete="current-password"
+              sx={{ backgroundColor: '#fff', borderRadius: '50px' }}
+            />
+
+            {/* Remember me */}
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" style={{ color: '#fff' }} />}
+              label={<Typography sx={{ color: '#fff' }}>Remember me</Typography>}
+            />
+            <Grid item xs sx={{ display: 'flex', justifyContent: 'end', alignItems: 'end', mt: -4 }} style={{ cursor: 'pointer' }}>
+              
+              <EnterMobileDialog open={dialogOpen} onClose={handleDialogToggle} onClick={handleClickOpen}/>
+                {/* <Typography variant="body2" sx={{ cursor: 'pointer', color: '#fff' }} onClick={handleClickOpen}>
+                Forgot Credentials
+              </Typography> */}
+            </Grid>
+            <Box sx={{ display: 'flex', alignItems: 'end', justifyContent: 'end', mt: 4 }}>
+              <ToggleGroupVariants />
+            </Box>
+            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+              <Button
+                type="submit"
+                variant="contained"
+                sx={{ mt: 3, mb: 2, backgroundColor: '#fff', color: '#364152b5', borderRadius: '50px', width: "150px", margin: '15px', display: 'flex', height: '45px' }}
+              >
+                LOGIN
+              </Button>
+            </Box>
+            <Grid item sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', mt: 3 }}>
+              <img src={CloudLogo} alt="Logo" />
+              <Typography sx={{ color: '#fff', mt: 1 }}>
+                Powered by
+              </Typography>
+              <Link href="http://www.shauryasoft.com" target="_blank" rel="noopener noreferrer" sx={{ color: '#fff', textDecoration: 'none' }}>
+                www.shauryasoft.com
+              </Link>
+            </Grid>
+          </Box>
+          <Box sx={{ mt: 2, display: 'flex', width: '100%', justifyContent: 'center', gap: '20px' }}>
+            <Button
+              variant="contained"
+              color="primary"
+              href="PLAY_STORE_LINK"
+              target="_blank"
+              rel="noopener noreferrer"
+              size="small"
+              sx={{ height: '50px', width: "150px", display: 'flex', backgroundColor: '#fff', color: '#000', borderRadius: '10px', lineHeight: '15px' }}
+              startIcon={<img src={PlayStoreImage} alt="Play Store" style={{ width: '20px', marginRight: '10px' }} />}
+            >
+              GET IT ON Google Play
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              href="APPLE_STORE_LINK"
+              target="_blank"
+              rel="noopener noreferrer"
+              size="small"
+              sx={{ height: '50px', width: "150px", display: 'flex', backgroundColor: '#fff', color: '#000', borderRadius: '10px', lineHeight: '15px' }}
+              startIcon={<img src={AppleStoreImage} alt="Apple Store" style={{ width: '25px', marginRight: '10px' }} />}
+            >
+              GET IT ON Apple Store
+            </Button>
+          </Box>
+          <Typography variant='subtitle1' color='#fff' sx={{ mt: 1 }}>
+            Online Fee Payment Policies/ Terms & Conditions
+          </Typography>
+        </Box>
+      </Grid>
+    </Grid>
+  </ThemeProvider>
+);
 }
