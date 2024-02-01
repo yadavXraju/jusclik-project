4415e613a0f96a176d3c1f0d0a36fd2c5e260d83
Vikash <vikash@shauryasoft.com>
2024-02-01 17:58:34 +0530

    Vikash (01/02/24)

--- src/views/sidebar-menus/fee-school-fee/ReceiptDetails.js
+++ src/views/sidebar-menus/fee-school-fee/ReceiptDetails.js
@@ -1,7 +1,9 @@
 
 import React from 'react'
+import timeTableData1 from '../timetable/TimeTableData'
 
 function ReceiptDetails() {
+  console.log(timeTableData1)
   return (
     <div>
       Transport
