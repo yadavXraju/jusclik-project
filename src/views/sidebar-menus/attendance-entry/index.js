9b9b5710e2a94427876c64f8ce48e767e1a8c67d
SuraJ <suraj@shauryasoft.com>
2024-02-02 18:24:43 +0530

    Attendance Entry component improved

--- src/views/sidebar-menus/attendance-entry/index.js
+++ src/views/sidebar-menus/attendance-entry/index.js
@@ -1,20 +1,5 @@
-import React, { useEffect } from 'react';
-import {
-  Box,
-  InputLabel,
-  MenuItem,
-  FormControl,
-  Select,
-  Divider,
-  Typography,
-  ListItem,
-  Avatar,
-  ListItemAvatar,
-  ListItemText,
-  Stack,
-  Paper,
-  Button,
-} from '@mui/material';
+import React from 'react';
+import {Box,InputLabel,MenuItem,FormControl,Select,Divider,Typography,ListItem,Avatar,ListItemAvatar,ListItemText,Stack,Paper,Button,} from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
@@ -24,6 +9,7 @@ import AvtarImg from '../../../assets/images/avatar.png';
 import { ClassList } from './ClassList';
 import { SectionList } from './SectionList';
 import { StudentList } from './StudentList';
+import TakeAttendance from './TakeAttendance'
 
 export default function AttendanceEntry() {
   const [selectClass, setSelectClass] = React.useState('');
@@ -48,35 +34,17 @@ export default function AttendanceEntry() {
   };
 
   const handleActionsClick = () => {
-    console.log('Selected Class:', selectClass);
-    console.log('Selected Section:', selectSection);
-
-    // Filter the student list based on the selected class and section
-    const filteredList = StudentList.filter(
-      (student) =>
-        student.class.trim().toLowerCase() === selectClass.trim().toLowerCase() &&
-        student.section.trim().toLowerCase() === selectSection.trim().toLowerCase()
-    );
-
-    console.log('Filtered List:', filteredList);
-
-    // Update the state directly with the filtered list
-    setFilteredStudentList(filteredList);
-    console.log('Filtered Student List:', filteredStudentList);
+    // Sort the entire student list alphabetically by student name
+    const StudentSortedList = [...StudentList].sort((a, b) => a.name.localeCompare(b.name));
+  
+    // Update the state directly with the sorted list
+    setFilteredStudentList(StudentSortedList);
   };
-
-  useEffect(() => {
-    console.log('Student List on Mount:', StudentList);
-  }, []);
-
-
-
-
 
   return (
     <Box>
       <Paper sx={{ borderRadius: '30px' }}>
-        <Box sx={{ minWidth: 250, display: 'flex', alignItems: 'baseline', p: 3 }}>
+        <Box sx={{ minWidth: 250, display: 'flex', alignItems: 'baseline', p: 3, justifyContent:'space-between' }}>
           <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DemoContainer components={['DatePicker']}>
               <DatePicker label="Select Date" slots={{ openPickerIcon: CalendarMonthTwoToneIcon }} />
@@ -124,63 +92,90 @@ export default function AttendanceEntry() {
             sx={{ width: '250px', padding: '10px', margin: '0 0 6px 8px' }}
             onClick={handleActionsClick}
           >
-            Actions
+            Search
           </Button>
+          <TakeAttendance/>
         </Box>
       </Paper>
       {/* Student List */}
       <Box sx={{ mt: 2 }}>
         <Paper sx={{ listStyleType: 'none', p: 0 }}>
+          <ListItem sx={{ display: 'flex', alignItems: 'center', p: 2 }}>
+            <ListItemText sx={{ flex: '0 0 15%' }}>
+              <Typography variant="h4" color="text.primary">
+                Admn No.
+              </Typography>
+            </ListItemText>
+            <ListItemText sx={{flex: '0 0 50%' }}>
+              <Typography variant="h4">Student Name</Typography>
+            </ListItemText>
+            <ListItemText sx={{ flex: '0 0 20%', justifyContent:'center' }}>
+              <Typography variant="h4" color="text.primary">
+                Status
+              </Typography>
+            </ListItemText>
+          </ListItem>
+          <Divider  />
+
           {(filteredStudentList.length > 0 ? filteredStudentList : StudentList).map((student) => (
             <React.Fragment key={student.id}>
               <ListItem sx={{ display: 'flex', alignItems: 'center', p: 2 }}>
-                <ListItemAvatar>
+
+                <ListItemText sx={{ flex: '0 0 15%' }}>
+                  <Typography variant="h4" color="text.primary">
+                    {student.admnNo}
+                  </Typography>
+                </ListItemText>
+
+                <ListItemAvatar sx={{ flex: '0 0 3%' }}>
                   <Avatar src={AvtarImg} sx={{ width: 50, height: 50 }} />
                 </ListItemAvatar>
-                <ListItemText sx={{ paddingLeft: '10px' }}>
+
+                <ListItemText sx={{ paddingLeft: '10px', flex: '0 0 32%' }}>
                   <Typography variant="h4">{student.name}</Typography>
-                  <React.Fragment>
-                    <Typography
-                      sx={{ display: 'inline' }}
-                      component="span"
-                      variant="subtitle2"
-                      color="text.primary"
-                    >
-                      {student.role}
-                    </Typography>
-                  </React.Fragment>
                 </ListItemText>
-                <Stack direction="row" spacing={2}>
+                
+                <ListItemText sx={{ flex: '0 0 40%' }}>
+                  <Typography variant="h4" color="text.secondary" >
+                   <Stack direction="row" spacing={2} sx={{ flex: '0 0 20%', justifyContent:'center' }}>
                   <Avatar
                     sx={{
-                      bgcolor: selectedAvatars[student.id] === 'P' ? '#7bc67b' : '#ccc',
+                      bgcolor: selectedAvatars[student.id] === 'H' ? '#7dceeb' : '#f1f1f1',
                       cursor: 'pointer',
+                      color:'#000000'
+                    }}
+                    onClick={() => handleAvatarClick('H', student.id)}
+                  >H</Avatar>
+                  <Avatar
+                    sx={{
+                      bgcolor: selectedAvatars[student.id] === 'P' ? '#7bc67b' : '#f1f1f1',
+                      cursor: 'pointer',
+                      color:'#000000'
                     }}
                     onClick={() => handleAvatarClick('P', student.id)}
-                  >
-                    P
-                  </Avatar>
+                  >P</Avatar>
                   <Avatar
                     sx={{
-                      bgcolor: selectedAvatars[student.id] === 'A' ? '#e2526b' : '#ccc',
+                      bgcolor: selectedAvatars[student.id] === 'A' ? '#e2526b' : '#f1f1f1',
                       cursor: 'pointer',
+                      color:'#000000'
                     }}
                     onClick={() => handleAvatarClick('A', student.id)}
-                  >
-                    A
-                  </Avatar>
+                  >A</Avatar>
                   <Avatar
                     sx={{
-                      bgcolor: selectedAvatars[student.id] === 'L' ? '#eeb058' : '#ccc',
+                      bgcolor: selectedAvatars[student.id] === 'L' ? '#eeb058' : '#f1f1f1',
                       cursor: 'pointer',
+                      color:'#000000'
                     }}
                     onClick={() => handleAvatarClick('L', student.id)}
-                  >
-                    L
-                  </Avatar>
+                  >L</Avatar>
                 </Stack>
+                  </Typography>
+                </ListItemText>
+
               </ListItem>
-              <Divider variant="middle" sx={{ width: '100%' }} component="li" />
+              <Divider/>
             </React.Fragment>
           ))}
         </Paper>
