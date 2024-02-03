472b7b03e544bb1a4a5f537492bc09c4735356c8
sangeeta <sangeeta@shauryasoft>
2024-02-03 16:19:19 +0530

    Marks entery pannel

--- src/views/sidebar-menus/marks-entry-panel/index.js
+++ src/views/sidebar-menus/marks-entry-panel/index.js
@@ -1,58 +1,169 @@
-import React from 'react';
-import { Box, Paper, Grid, Typography, Stack, TextField } from '@mui/material';
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+import { makeStyles, useTheme, ThemeProvider } from '@mui/styles';
 
-const MarksEntryPanel = () => {
+const useStyles = makeStyles((theme) => ({
+  tableContainer: {
+    marginBottom: theme.spacing(2),
+  },
+  tableHead: {
+    background: theme.palette.background.default,
+    zIndex: 1000,
+  },
+  tableHeadCell: {
+    fontWeight: 'bold',
+    position: 'sticky',
+    top: 0,
+    background: theme.palette.background.default,
+
+  },
+  rowHover: {
+    '&:hover': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+  fixedColumn: {
+    left: 0,
+    position: 'sticky',
+    zIndex:999,
+    backgroundColor: theme.palette.background.default,
+  },
+  fixedIstColumn: {
+    left: 0,
+    position: 'sticky',
+    zIndex:9999,
+    backgroundColor: theme.palette.background.default,
+  },
+}));
+
+
+function createData(name, admissionNo, english, hindi, math, science, socialScience, physics) {
+  return { name, admissionNo, english, hindi, math, science, socialScience, physics };
+}
+
+const rows = [
+  createData('Sangeeta', "1323", 159, 6.0, 24, 4.0, 8.0, 5.0),
+  createData('Anjali', "123", 237, 9.0, 37, 4.3, 6.0, 7.0),
+  createData('Aman', "2434", 262, 16.0, 24, 6.0, 9.0, 8.0),
+  createData('Jivesh', "24222", 305, 3.7, 67, 4.3, 5.0, 6.0),
+  createData('Gingerbread', "@$44", 356, 16.0, 49, 3.9, 7.0, 9.0),
+  createData('Gingerbread', "@$44", 356, 16.0, 49, 3.9, 7.0, 9.0),
+  createData('Gingerbread', "@$44", 356, 16.0, 49, 3.9, 7.0, 9.0),
+  createData('Gingerbread', "@$44", 356, 16.0, 49, 3.9, 7.0, 9.0), createData('Gingerbread', "@$44", 356, 16.0, 49, 3.9, 7.0, 9.0), createData('Gingerbread', "@$44", 356, 16.0, 49, 3.9, 7.0, 9.0), createData('Gingerbread', "@$44", 356, 16.0, 49, 3.9, 7.0, 9.0),
+];
+
+export default function MarksEntryPanel() {
+  const theme = useTheme();
+  const classes = useStyles(theme);
+
   return (
-    <>
-      <Box>
-        <Paper>
-          <Stack direction="row" spacing={2}>
-            <Typography>
-              Student Name:<br />
-              Admission ID:
-            </Typography>
+    <ThemeProvider theme={theme}>
+      <Paper sx={{ width: '100%' }}>
+        <TableContainer sx={{ maxHeight: 640 }}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+              <TableCell  className={classes.fixedIstColumn} sx={{top:'0'}} >Student ID</TableCell>
+              <TableCell align="left">ENGLISH</TableCell>
+              <TableCell align="left">ENGLISH</TableCell>
+              <TableCell align="left">ENGLISH</TableCell>
+              <TableCell align="left">ENGLISH</TableCell>
+              <TableCell align="left">ENGLISH</TableCell>
+              <TableCell align="left">ENGLISH</TableCell>
+              <TableCell align="left">ENGLISH</TableCell>
+              <TableCell align="left">ENGLISH</TableCell>
+              <TableCell align="left">ENGLISH</TableCell>
+              <TableCell align="left">ENGLISH</TableCell>
+              <TableCell align="left">ENGLISH</TableCell>
+              <TableCell align="left">ENGLISH</TableCell>
+              <TableCell align="left">ENGLISH</TableCell>
+              <TableCell align="left">ENGLISH</TableCell>
+              <TableCell align="left">ENGLISH</TableCell>
+              <TableCell align="left">ENGLISH</TableCell>
+              <TableCell align="left">ENGLISH</TableCell>
+              <TableCell align="left">ENGLISH</TableCell>
+              <TableCell align="left">ENGLISH</TableCell>
 
-            <Grid md={2}>English (50)</Grid>
-            <Grid>Hindi (50)</Grid>
-            <Grid>Punjabi (50)</Grid>
-            <Grid>Punjabi (50)</Grid>
-            <Grid>Punjabi (50)</Grid>
-            <Grid>Punjabi (50)</Grid>
-            <Grid>Punjabi (50)</Grid>
-          </Stack>
-
-          <Stack direction="row" spacing={2}>
-            <Typography>
-              Sangeeta<br />
-              131425
-            </Typography>
-
-            <Grid  md={2}>
-              <TextField required id="outlined-required" label="English" />
-            </Grid>
-            <Grid>
-              <TextField required id="outlined-required" label="Hindi" />
-            </Grid>
-            <Grid>
-              <TextField required id="outlined-required" label="Punjabi" />
-            </Grid>
-            <Grid>
-              <TextField required id="outlined-required" label="Punjabi" />
-            </Grid>
-            <Grid>
-              <TextField required id="outlined-required" label="Punjabi" />
-            </Grid>
-            <Grid>
-              <TextField required id="outlined-required" label="Punjabi" />
-            </Grid>
-            <Grid>
-              <TextField required id="outlined-required" label="Punjabi" />
-            </Grid>
-          </Stack>
-        </Paper>
-      </Box>
-    </>
+             
+              </TableRow>
+            </TableHead>
+            <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.name} >
+                <TableCell className={classes.fixedColumn} >{`${row.name} (${row.admissionNo})`}</TableCell>
+                <TableCell align="left">
+                  <TextField fullWidth variant="outlined" size="small" sx={{ width: '60px' }} />
+                </TableCell>
+                <TableCell align="left">
+                  <TextField fullWidth variant="outlined" size="small" sx={{ width: '60px' }} />
+                </TableCell>
+                <TableCell align="left">
+                  <TextField fullWidth variant="outlined" size="small" sx={{ width: '60px' }} />
+                </TableCell>
+                <TableCell align="left">
+                  <TextField fullWidth variant="outlined" size="small" sx={{ width: '60px' }} />
+                </TableCell>
+                <TableCell align="left">
+                  <TextField fullWidth variant="outlined" size="small" sx={{ width: '60px' }} />
+                </TableCell>
+                <TableCell align="left">
+                  <TextField fullWidth variant="outlined" size="small" sx={{ width: '60px' }} />
+                </TableCell>
+                <TableCell align="left">
+                  <TextField fullWidth variant="outlined" size="small" sx={{ width: '60px' }} />
+                </TableCell>
+                <TableCell align="left">
+                  <TextField fullWidth variant="outlined" size="small" sx={{ width: '60px' }} />
+                </TableCell>
+                <TableCell align="left">
+                  <TextField fullWidth variant="outlined" size="small" sx={{ width: '60px' }} />
+                </TableCell>
+                <TableCell align="left">
+                  <TextField fullWidth variant="outlined" size="small" sx={{ width: '60px' }} />
+                </TableCell>
+                <TableCell align="left">
+                  <TextField fullWidth variant="outlined" size="small" sx={{ width: '60px' }} />
+                </TableCell>
+                <TableCell align="left">
+                  <TextField fullWidth variant="outlined" size="small" sx={{ width: '60px' }} />
+                </TableCell>
+                <TableCell align="left">
+                  <TextField fullWidth variant="outlined" size="small" sx={{ width: '60px' }} />
+                </TableCell>
+                <TableCell align="left">
+                  <TextField fullWidth variant="outlined" size="small" sx={{ width: '60px' }} />
+                </TableCell>
+                <TableCell align="left">
+                  <TextField fullWidth variant="outlined" size="small" sx={{ width: '60px' }} />
+                </TableCell>
+                <TableCell align="left">
+                  <TextField fullWidth variant="outlined" size="small" sx={{ width: '60px' }} />
+                </TableCell>
+                <TableCell align="left">
+                  <TextField fullWidth variant="outlined" size="small" sx={{ width: '60px' }} />
+                </TableCell>
+                <TableCell align="left">
+                  <TextField fullWidth variant="outlined" size="small" sx={{ width: '60px' }} />
+                </TableCell>
+                <TableCell align="left">
+                  <TextField fullWidth variant="outlined" size="small" sx={{ width: '60px' }} />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+    </ThemeProvider>
   );
-};
+}
 
-export default MarksEntryPanel;
+
+
