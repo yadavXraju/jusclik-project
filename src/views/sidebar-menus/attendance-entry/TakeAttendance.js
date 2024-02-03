eba7e52a2ba17aa1727fbfdb742d46a2bef5a82d
SuraJ <suraj@shauryasoft.com>
2024-02-03 17:27:14 +0530

    Some functionality added in Attendance antry

--- src/views/sidebar-menus/attendance-entry/TakeAttendance.js
+++ src/views/sidebar-menus/attendance-entry/TakeAttendance.js
@@ -28,6 +28,7 @@ export default function ActionsPopover() {
     const open = Boolean(anchorEl);
     const id = open ? 'simple-popover' : undefined;
 
+    
     return (
         <div>
             <Button aria-describedby={id} variant="outlined" size="small" onClick={handleClick} sx={{ padding: '10px', margin: '0 0 6px 8px', width: '100px' }}>
@@ -47,28 +48,28 @@ export default function ActionsPopover() {
                     sx={{ width: '100%', bgcolor: 'background.paper' }}
                     aria-label="contacts"
                 >
-                    <ListItem disablePadding>
+                    <ListItem disablePadding onClick={() => handleClickOpen()}>
                         <ListItemButton>
-                            <ListItemText id='1' primary="Mark all holiday" onClick={() => handleClickOpen("Mark all holiday")} />
+                            <ListItemText id='1' primary="Mark all holiday" />
                         </ListItemButton>
                     </ListItem>
 
-                    <ListItem disablePadding>
+                    <ListItem disablePadding onClick={() => handleClickOpen()}>
                         <ListItemButton >
-                            <ListItemText id='2' primary="Mark all present" onClick={() => handleClickOpen("Mark all present")} />
+                            <ListItemText id='2' primary="Mark all present" />
                         </ListItemButton>
                     </ListItem>
 
-                    <ListItem disablePadding>
+                    <ListItem disablePadding onClick={() => handleClickOpen()} >
                         <ListItemButton>
-                            <ListItemText id='3' primary="Mark all absent" onClick={() => handleClickOpen("Mark all absent")} />
+                            <ListItemText id='3' primary="Mark all absent" />
                         </ListItemButton>
                     </ListItem>
                 </List>
-                <ConfirmationDialog 
-                open={isConfirmationDialogOpen} 
-                onClose={handleComposeClose} 
-                action={selectedAction} 
+                <ConfirmationDialog
+                    open={isConfirmationDialogOpen}
+                    onClose={handleComposeClose}
+                    action={selectedAction}
                 />
             </Popover>
         </div>
