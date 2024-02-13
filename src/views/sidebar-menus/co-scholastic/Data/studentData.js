ad4e95ad796c6623fac77ee70ef3145f9537a703
sangeeta <sangeeta@shauryasoft.com>
2024-02-13 17:59:45 +0530

    commit select classes working

---
+++ src/views/sidebar-menus/co-scholastic/Data/studentData.js
@@ -0,0 +1,61 @@
+export  const studentData=[ {
+    id: 0,
+    value: "0",
+    label: "All",
+
+  },
+    {
+      id: 1,
+      value: "1",
+      label: "Sangeeta",
+      name: 'Sangeeta',
+     admissionNo: '1323'
+    },
+    {
+      id: 2,
+      value: "2",
+      label: "Anjali",
+      name: 'Anjali', admissionNo: '123'
+    },
+    {
+      id: 3,
+      value: "3",
+      label: "Aman",
+      name: 'Aman', admissionNo: '2434' 
+    },
+    {
+      id: 4,
+      value: "5",
+      label: "Laksh",
+      name: 'Laksh', admissionNo: '2634'
+    },
+    {
+      id: 5,
+      value: "6",
+      label: "Archita",
+      name: 'Archita', admissionNo: '2634'
+    },
+    {
+      id: 6,
+      value: "7",
+      label: "Dexter",
+      name: 'Dexter', admissionNo: '2634'
+    },
+    {
+      id: 7,
+      value: "8",
+      label: "Suraj",
+      name: 'Suraj', admissionNo: '2634'
+    },
+    {
+      id: 8,
+      value: "9",
+      label: "Mixter",
+      name: 'Mixter', admissionNo: '2634'
+    },
+    {
+        id: 9,
+        value: "10",
+        label: "riya",
+        name: 'riya', admissionNo: '2634'
+      },]
