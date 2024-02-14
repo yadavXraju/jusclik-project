4d1c832c7327288f826fc618cee0c6f406d44b32
vikash <vikash@shauryasoft.com>
2024-02-14 17:43:58 +0530

    leave Drawer updated

--- src/views/sidebar-menus/leave-application/LeaveApplicationFilter.js
+++ src/views/sidebar-menus/leave-application/LeaveApplicationFilter.js
@@ -8,31 +8,32 @@ import Popover from '@mui/material/Popover';
 import { IconButton } from '@mui/material';
 import PopupState, { bindTrigger, bindPopover } from 'material-ui-popup-state';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import { Divider, ListItem, ListItemText } from '@mui/material';
+//import { Divider } from '@mui/material';
 import TablePagination from '@mui/material/TablePagination'; // Import TablePagination component
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 
 // Leave data
 const leavedata = [
-  { Appno: 2723, leavetype: 'Casual Leave', Appdate: '02.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '2 fullday', status: 'Pending' },
-  { Appno: 2724, leavetype: 'Casual Leave', Appdate: '08.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '1 fullday', status: 'Approved' },
-  { Appno: 2723, leavetype: 'Casual Leave', Appdate: '02.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '2 fullday', status: 'Pending' },
-  { Appno: 2724, leavetype: 'Casual Leave', Appdate: '08.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '1 fullday', status: 'Approved' },
-  { Appno: 2723, leavetype: 'Casual Leave', Appdate: '02.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '2 fullday', status: 'Pending' },
-  { Appno: 2724, leavetype: 'Casual Leave', Appdate: '08.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '1 fullday', status: 'Approved' },
-  { Appno: 2723, leavetype: 'Casual Leave', Appdate: '02.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '2 fullday', status: 'Pending' },
-  { Appno: 2724, leavetype: 'Casual Leave', Appdate: '08.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '1 fullday', status: 'Approved' },
-  { Appno: 2723, leavetype: 'Casual Leave', Appdate: '02.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '2 fullday', status: 'Pending' },
-  { Appno: 2724, leavetype: 'Casual Leave', Appdate: '08.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '1 fullday', status: 'Approved' },
-  { Appno: 2723, leavetype: 'Casual Leave', Appdate: '02.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '2 fullday', status: 'Pending' },
-  { Appno: 2724, leavetype: 'Casual Leave', Appdate: '08.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '1 fullday', status: 'Approved' },
-  { Appno: 2723, leavetype: 'Casual Leave', Appdate: '02.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '2 fullday', status: 'Pending' },
-  { Appno: 2724, leavetype: 'Casual Leave', Appdate: '08.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '1 fullday', status: 'Approved' },
-  { Appno: 2723, leavetype: 'Casual Leave', Appdate: '02.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '2 fullday', status: 'Pending' },
-  { Appno: 2724, leavetype: 'Casual Leave', Appdate: '08.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '1 fullday', status: 'Approved' },
-  { Appno: 2723, leavetype: 'Casual Leave', Appdate: '02.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '2 fullday', status: 'Pending' },
-  { Appno: 2724, leavetype: 'Casual Leave', Appdate: '08.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '1 fullday', status: 'Approved' },
-  { Appno: 2723, leavetype: 'Casual Leave', Appdate: '02.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '2 fullday', status: 'Pending' },
-  { Appno: 2724, leavetype: 'Casual Leave', Appdate: '08.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '1 fullday', status: 'Approved' },
+  { Appno: 2723, leavetype: 'Casual Leave', Appdate: '02.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '2 Fullday', status: 'Pending' },
+  { Appno: 2724, leavetype: 'Earned Leave', Appdate: '08.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '1 Fullday', status: 'Approved' },
+  { Appno: 2723, leavetype: 'Medical Leave', Appdate: '02.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: 'Short Leave(SL)', status: 'Pending' },
+  { Appno: 2724, leavetype: 'Casual Leave', Appdate: '08.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: 'Half Day(H1)', status: 'Approved' },
+  { Appno: 2723, leavetype: 'Earned Leave', Appdate: '02.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '2 Fullday', status: 'Pending' },
+  { Appno: 2724, leavetype: 'Without Pay Leave', Appdate: '08.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '1 Fullday', status: 'Approved' },
+  { Appno: 2723, leavetype: 'Earned Leave', Appdate: '02.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: 'Half Day(H1)', status: 'Pending' },
+  { Appno: 2724, leavetype: 'Medical Leave', Appdate: '08.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: 'Half Day(H1)', status: 'Approved' },
+  { Appno: 2723, leavetype: 'Medical Leave', Appdate: '02.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: 'Short Leave(SL)', status: 'Pending' },
+  { Appno: 2724, leavetype: 'Casual Leave', Appdate: '08.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: 'Half Day(H1)', status: 'Approved' },
+  { Appno: 2723, leavetype: 'Casual Leave', Appdate: '02.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '2 Fullday', status: 'Pending' },
+  { Appno: 2724, leavetype: 'Medical Leave', Appdate: '08.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: 'Short Leave(SL)', status: 'Approved' },
+  { Appno: 2723, leavetype: 'Casual Leave', Appdate: '02.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: 'Half Day(H1)', status: 'Pending' },
+  { Appno: 2724, leavetype: 'Without Pay Leave', Appdate: '08.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '1 Fullday', status: 'Approved' },
+  { Appno: 2723, leavetype: 'Earned Leave', Appdate: '02.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '2 Fullday', status: 'Pending' },
+  { Appno: 2724, leavetype: 'Medical Leave', Appdate: '08.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: 'Half Day(H1)', status: 'Approved' },
+  { Appno: 2723, leavetype: 'Earned Leave', Appdate: '02.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: 'Short Leave(SL)', status: 'Pending' },
+  { Appno: 2724, leavetype: 'Casual Leave', Appdate: '08.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: 'Half Day(H1)', status: 'Approved' },
+  { Appno: 2723, leavetype: 'Earned Leave', Appdate: '02.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: '2 Fullday', status: 'Pending' },
+  { Appno: 2724, leavetype: 'Medical Leave', Appdate: '08.Feb.2024', Details: 'Dear Sir/Ma`am, I am not feeling well today', plan: 'Planned', applied: 'Half Day(H1)', status: 'Approved' },
 ];
 export default function LeaveTabs() {
   const [value, setValue] = useState(0);
@@ -67,113 +68,103 @@ export default function LeaveTabs() {
   return (
     <Box sx={{ width: '100%', bgcolor: 'background.paper', mb: 2 }}>
       <Tabs sx={{ mb: 2 }} value={value} onChange={handleChange}>
-        
+
         <Tab label="Pending" />
         <Tab label="Approved" />
         <Tab label="All" />
       </Tabs>
 
-      <Box sx={{ overflowX: 'auto' }}>
-        <Paper sx={{ listStyleType: 'none', p: 0 }}>
-          <ListItem sx={{ display: 'flex', alignItems: 'center', p: 2 }}>
-            {/* Your list item headers */}
-            <ListItemText sx={{ flex: '0 0 10%' }}>
-              <Typography variant="h4" >
+      <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>
+              <Typography variant="h4">
                 App No
               </Typography>
-            </ListItemText>
-            <ListItemText sx={{ flex: '0 0 10%' }}>
+            </TableCell>
+            <TableCell>
               <Typography variant="h4">Leave Type</Typography>
-            </ListItemText>
-            <ListItemText sx={{ flex: '0 0 10%' }}>
+            </TableCell>
+            <TableCell>
               <Typography variant="h4">
                 App Date
               </Typography>
-            </ListItemText>
-            <ListItemText sx={{ flex: '0 0 30%' }}>
+            </TableCell>
+            <TableCell>
               <Typography variant="h4">
                 Details
               </Typography>
-            </ListItemText>
-            <ListItemText sx={{ flex: '0 0 10%' }}>
+            </TableCell>
+            <TableCell>
               <Typography variant="h4">
                 Planned / Unplanned
               </Typography>
-            </ListItemText>
-            <ListItemText sx={{ flex: '0 0 10%' }}>
+            </TableCell>
+            <TableCell>
               <Typography variant="h4">
                 Applied
               </Typography>
-            </ListItemText>
-            <ListItemText sx={{ flex: '0 0 10%' }}>
+            </TableCell>
+            <TableCell>
               <Typography variant="h4">
                 Status
               </Typography>
-            </ListItemText>
-            <ListItemText sx={{ flex: '0 0 10%', display: 'flex', justifyContent: 'center' }}>
-              <Typography variant="h4">
+            </TableCell>
+            <TableCell>
+              <Typography variant="h4" sx={{ display: 'flex', justifyContent: 'center' }}>
                 Action
               </Typography>
-            </ListItemText>
-          </ListItem>
-          <Divider />
-        </Paper>
-
-        {filteredData
-          .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-          .map((item, index) => (
-            <Paper sx={{ listStyleType: 'none', p: 0 }} key={index}>
-              <ListItem sx={{ display: 'flex', alignItems: 'center', p: 2 }}>
-                <ListItemText sx={{ flex: '0 0 10%' }}>
-                  <Typography variant="h5" >
-                    {item.Appno}
-                  </Typography>
-                </ListItemText>
-                <ListItemText sx={{ flex: '0 0 10%' }}>
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {filteredData
+            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            .map((item, index) => (
+              <TableRow key={index}>
+                <TableCell>
+                  <Typography variant="h5">{item.Appno}</Typography>
+                </TableCell>
+                <TableCell>
                   <Typography variant="h5">{item.leavetype}</Typography>
-                </ListItemText>
-                <ListItemText sx={{ flex: '0 0 10%' }}>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="h5">{item.Appdate}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="h5" sx={{minWidth:'200px'}}>{item.Details}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="h5">{item.plan}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography variant="h5">{item.applied}</Typography>
+                </TableCell>
+                <TableCell>
                   <Typography variant="h5">
-                    {item.Appdate}
-                  </Typography>
-                </ListItemText>
-                <ListItemText sx={{ flex: '0 0 30%' }}>
-                  <Typography variant="h5">
-                    {item.Details}
-                  </Typography>
-                </ListItemText>
-                <ListItemText sx={{ flex: '0 0 10%' }}>
-                  <Typography variant="h5">
-                    {item.plan}
-                  </Typography>
-                </ListItemText>
-                <ListItemText sx={{ flex: '0 0 10%' }}>
-                  <Typography variant="h5">
-                    {item.applied}
-                  </Typography>
-                </ListItemText>
-                <ListItemText sx={{ flex: '0 0 10%' }}>
-                  <Typography variant="h5">
-                    <Box sx={{
-                      color: item.status === 'Approved' ? 'rgb(0, 200, 83)' : (item.status === 'overdue' ? '#433e1a' : 'rgb(216, 67, 21)'),
-                      borderRadius: '16px',
-                      background: item.status === 'Approved' ? 'rgba(147, 213, 166, 0.38)' : (item.status === 'overdue' ? '#ffff003b' : 'rgb(251, 233, 231)'),
-                      '&:hover': {
-                        background: item.status === 'Approved' ? 'rgb(0, 200, 83)' : (item.status === 'overdue' ? '#b4aa10' : 'rgb(216, 67, 21)'),
-                        color: 'white',
-                      },
-                      width: '70%',
-                      padding: '2px',
-                      paddingRight: '8px',
-                      paddingLeft: '8px',
-                      textAlign: 'center'
-                    }}>
+                    <Box
+                      sx={{
+                        color: item.status === 'Approved' ? 'rgb(0, 200, 83)' : (item.status === 'overdue' ? '#433e1a' : 'rgb(216, 67, 21)'),
+                        borderRadius: '16px',
+                        background: item.status === 'Approved' ? 'rgba(147, 213, 166, 0.38)' : (item.status === 'overdue' ? '#ffff003b' : 'rgb(251, 233, 231)'),
+                        '&:hover': {
+                          background: item.status === 'Approved' ? 'rgb(0, 200, 83)' : (item.status === 'overdue' ? '#b4aa10' : 'rgb(216, 67, 21)'),
+                          color: 'white',
+                        },
+                        width: "100%",
+                        padding: '2px',
+                        paddingRight: '8px',
+                        paddingLeft: '8px',
+                        textAlign: 'center'
+                      }}
+                    >
                       {item.status}
                     </Box>
                   </Typography>
-                </ListItemText>
-                <ListItemText sx={{ flex: '0 0 10%', display: 'flex', justifyContent: 'center' }}>
-                  <Typography variant="h4">
+                </TableCell>
+                <TableCell>
+                  <Typography variant="h4" sx={{ display: 'flex', justifyContent: 'center' }}>
                     <PopupState variant="popover" popupId="demo-popup-popover">
                       {(popupState) => (
                         <div>
@@ -201,12 +192,13 @@ export default function LeaveTabs() {
                       )}
                     </PopupState>
                   </Typography>
-                </ListItemText>
-              </ListItem>
-              <Divider />
-            </Paper>
-          ))}
-      </Box>
+                </TableCell>
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
       {/* ======== Render TablePagination component  ========== */}
       <TablePagination
         component="div"
