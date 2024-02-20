632209c984dd3d2963e07fef9e37c644f97e7d82
ruby <ruby@shauryasoft.com>
2024-02-20 17:51:56 +0530

    upload gallery

---
+++ src/views/sidebar-menus/upload-photogallery/Upload-gallery-data.js
@@ -0,0 +1,25 @@
+const UploadGallerydata = [
+    {
+      albumName: 'Photogallery Album-1',
+      hidden: 'No',
+      type: 'Photo',
+      lastModified: '02/05/2024',
+      updateby: 'Ruby',
+    },
+    {
+        albumName: 'Photogallery Album-2',
+        hidden: 'No',
+        type: 'Photo',
+        lastModified: '02/05/2024',
+        updateby: 'Ruby',
+      },
+      {
+        albumName: 'Photogallery Album-3',
+        hidden: 'No',
+        type: 'Photo',
+        lastModified: '02/05/2024',
+        updateby: 'Ruby',
+      },
+    // Add more homework data objects as needed
+  ];
+export default UploadGallerydata;
