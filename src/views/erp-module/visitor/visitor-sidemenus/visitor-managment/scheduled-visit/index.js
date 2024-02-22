09772776964991d132e04b9ee2256bbeebc96fb5
AnkushGupta <ankush@shauryasoft>
2024-02-22 17:58:47 +0530

    scheduled visit page done

--- src/views/erp-module/visitor/visitor-sidemenus/visitor-managment/scheduled-visit/index.js
+++ src/views/erp-module/visitor/visitor-sidemenus/visitor-managment/scheduled-visit/index.js
@@ -1,9 +1,450 @@
-import React from 'react'
+import React from 'react';
+import { useState, useRef } from 'react';
+import { Chip, Grid, IconButton, Paper, Box, Button, Typography, Drawer } from '@mui/material';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import CheckBoxOutlinedIcon from '@mui/icons-material/CheckBoxOutlined';
+import { TextField } from '@mui/material';
+import Autocomplete from '@mui/material/Autocomplete';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { Delete } from '@mui/icons-material';
+import { styled } from '@mui/material/styles';
+import PeopleOutlineOutlinedIcon from '@mui/icons-material/PeopleOutlineOutlined';
+import VisitorDrawer from '../visitor-entry/rightDrawer';
+import PrintIcon from '@mui/icons-material/Print';
+import { useReactToPrint } from 'react-to-print';
+import AvatarImage from '../../../../../../assets/images/avatar.png';
+
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1
+});
+
+//sample data
+const hundredOptions = [
+  { label: 'Aamir Khan', year: 1994, id: '686712729', index: 0 },
+  { label: 'Shah Rukh Khan', year: 1972, id: '446712729', index: 1 },
+  { label: 'Salman Khan', year: 1974, id: '126712729', index: 2 },
+  { label: 'Rajinikanth', year: 2008, id: '456712729', index: 3 },
+  { label: 'Amitabh Bachchan', year: 1957, id: '906712729', index: 4 },
+  { label: 'Priyanka Chopra', year: 1993, id: '766712729', index: 5 },
+  { label: 'Deepika Padukone', year: 1994, id: '534712729', index: 6 },
+  { label: 'Hrithik Roshan', year: 2003, id: '68673652729', index: 7 },
+  { label: 'Kareena Kapoor Khan', year: 1966, id: '7626712729', index: 8 },
+  { label: 'Akshay Kumar', year: 1999, id: '621812729', index: 9 },
+  { label: 'Aishwarya', year: 2001, id: '123712729', index: 10 },
+  { label: 'Ranbir Kapoor', year: 1980, id: '2762112729', index: 11 },
+  { label: 'Alia Bhatt', year: 1994, id: '216112729', index: 12 },
+  { label: 'Ranveer Singh', year: 2010, id: '261712729', index: 13 },
+  { label: 'Katrina Kaif', year: 2002, id: '127712729', index: 14 },
+  { label: 'Varun Dhawan', year: 1975, id: '7906712729', index: 15 }
+];
+
+const ComponentToPrint = React.forwardRef((props, ref) => {
+  return (
+    <div ref={ref}>
+      <div>
+        <div>
+          <h2>{props.school.name}</h2>
+          <p>{props.school.address}</p>
+          <p>{props.school.phone}</p>
+        </div>
+        <div>
+          <h4>Visitor slip</h4>
+        </div>
+        <div>
+          <div>
+            <ul>
+              <li>{`Visitor No: ${props.user.id}`}</li>
+              <li>{`Visitor Name: ${props.user.label}`}</li>
+              <li>{`Purpose: ${props.user.id}`}</li>
+              <li>{`To meet: ${props.user.id}`}</li>
+              <li>{`Address:  ${props.user.id}`}</li>
+              <li>{`Category: ${props.user.id}`}</li>
+              <li>{`Remarks: ${props.user.id}`}</li>
+              <li>{`Entry date: ${props.user.id}`}</li>
+              <li>{`Phone: ${props.user.id}`}</li>
+              <li>{`Time in: ${props.user.id}`}</li>
+            </ul>
+          </div>
+          <div>
+            <img src={AvatarImage} alt="avatar" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+});
 
 const ScheduledVisit = () => {
-  return (
-    <div> Scheduled Visit </div>
-  )
-}
+  const [template, setTemplate] = useState('');
+  const [selectedID, setSelectedID] = useState(null);
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
+  const [state, setState] = useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false
+  });
+  const [value, setValue] = useState(hundredOptions[0]);
+  const [inputValue, setInputValue] = useState('');
 
-export default ScheduledVisit
+  const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const handleChange = (event) => {
+    setTemplate(event.target.value);
+  };
+
+  const handleIDChange = (event) => {
+    setSelectedID(event.target.files[0]);
+  };
+  const handlePhotoChange = (event) => {
+    setSelectedPhoto(event.target.files[0]);
+  };
+
+  const handleDeleteFileID = () => {
+    setSelectedID(null);
+  };
+  const handleDeletePhoto = () => {
+    setSelectedPhoto(null);
+  };
+
+  const handleFirst = () => {
+    setValue(hundredOptions[0]);
+  };
+  const handlePrevious = (index) => {
+    const finalIndex = index == 0 ? hundredOptions.length - 1 : index - 1;
+    setValue(hundredOptions[finalIndex]);
+  };
+  const handleNext = (index) => {
+    const finalIndex = index == hundredOptions.length - 1 ? 0 : index + 1;
+    setValue(hundredOptions[finalIndex]);
+  };
+  const handleLast = () => {
+    setValue(hundredOptions[hundredOptions.length - 1]);
+  };
+
+  return (
+    <>
+      <Grid container spacing={4} sx={{ marginTop: '20px' }}>
+        <Grid item xs={10}>
+          <Paper>
+            <Grid container spacing={3} sx={{ p: '25px' }}>
+              <Grid item xs={6}>
+                <Grid container spacing={2} sx={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+                  <Grid item xs={4}>
+                    <Chip
+                      label={value?.id}
+                      onClick={() => {
+                        console.log('doing something');
+                      }}
+                      sx={{ p: 1, width: '100%' }}
+                      color="primary"
+                    />
+                  </Grid>
+                  <Grid item xs={5}>
+                    <Autocomplete
+                      disablePortal
+                      id="combo-box-demo"
+                      options={hundredOptions}
+                      renderInput={(params) => <TextField {...params} label="Visiters" />}
+                      value={value}
+                      onChange={(event, newValue) => {
+                        // console.log(event.target.index)
+                        console.log(newValue);
+                        setValue(newValue);
+                      }}
+                      inputValue={inputValue}
+                      onInputChange={(event, newValue) => {
+                        setInputValue(newValue);
+                      }}
+                    />
+                  </Grid>
+                  <Grid item xs={3}>
+                    <Box sx={{ marginLeft: '10px' }}>
+                      <IconButton sx={{ p: 0 }} id="first" onClick={() => handleFirst()}>
+                        <SkipPreviousIcon sx={{ fontSize: '25px' }} />
+                      </IconButton>
+                      <IconButton sx={{ p: 0 }} id="previous" onClick={() => handlePrevious(value.index)}>
+                        <NavigateBeforeIcon sx={{ fontSize: '25px' }} />
+                      </IconButton>
+                      <IconButton sx={{ p: 0 }} id="next" onClick={() => handleNext(value.index)}>
+                        <NavigateNextIcon sx={{ fontSize: '25px' }} />
+                      </IconButton>
+                      <IconButton sx={{ p: 0 }} id="last" onClick={() => handleLast()}>
+                        <SkipNextIcon sx={{ fontSize: '25px' }} />
+                      </IconButton>
+                    </Box>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={6}>
+                <Grid container spacing={2} sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                  <Grid item xs={8}>
+                    <Box>
+                      <TextField
+                        id="outlined-select-option"
+                        type="date"
+                        fullWidth
+                        label="Entry Date"
+                        sx={{}}
+                        InputLabelProps={{
+                          shrink: true
+                        }}
+                      />
+                    </Box>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <Box>
+                      <Chip
+                        icon={<CheckBoxOutlinedIcon sx={{ color: 'green !important' }} />}
+                        label="1874 Total visitors"
+                        variant="outlined"
+                        sx={{ fontSize: '12px' }}
+                      />
+                    </Box>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={6}>
+                <Grid container spacing={2} sx={{display: 'flex', justifyContent: "space-around", alignItems: "center"}}>
+                  <Grid item xs={8}>
+                    <TextField id="outlined-basic" label="phone" variant="outlined" fullWidth inputProps={{ maxLength: 10 }} />
+                  </Grid>
+                  <Grid item xs={4}>
+                    <Chip label="Security Code: 86865" />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={6}>
+                <Grid container spacing={3}>
+                  <Grid item xs={6}>
+                    <TextField
+                      id="outlined-select-option"
+                      type="date"
+                      fullWidth
+                      label="Visit date"
+                      sx={{}}
+                      InputLabelProps={{
+                        shrink: true
+                      }}
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <TextField
+                      id="outlined-select-option"
+                      type="time"
+                      fullWidth
+                      label="Visit time"
+                      sx={{}}
+                      InputLabelProps={{
+                        shrink: true
+                      }}
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={6}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">Category</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={template}
+                    label="Category"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value={1}>Interview</MenuItem>
+                    <MenuItem value={2}>Meeting</MenuItem>
+                    <MenuItem value={3}>Parents Meeting</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={6}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">Accompanied by</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={template}
+                    label="Accompanied by"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value={1}>1</MenuItem>
+                    <MenuItem value={2}>2</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                    <MenuItem value={3}>None</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={6}>
+                <TextField id="outlined-basic" label="Addresss (R)" variant="outlined" fullWidth />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField id="outlined-basic" label="Remarks" variant="outlined" fullWidth />
+              </Grid>
+              <Grid item xs={6}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">Purpose</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={template}
+                    label="Purpose"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value={1}>Interview</MenuItem>
+                    <MenuItem value={2}>Meeting</MenuItem>
+                    <MenuItem value={3}>Parents Meetingh</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+                <Box>
+                  <Box>
+                    <Button component="label" role={undefined} variant="contained" tabIndex={-1} startIcon={<CloudUploadIcon />}>
+                      Upload Photo
+                      <VisuallyHiddenInput type="file" accept="image/*" onChange={handlePhotoChange} />
+                    </Button>
+                  </Box>
+                  {selectedPhoto != null ? (
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                      <Typography
+                        variant="body1"
+                        sx={{ width: '100px', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}
+                      >
+                        {selectedPhoto?.name}
+                      </Typography>
+                      <IconButton color="primary" onClick={handleDeletePhoto}>
+                        <Delete />
+                      </IconButton>
+                    </Box>
+                  ) : null}
+                </Box>
+                <Box>
+                  <Box>
+                    <Button
+                      component="label"
+                      color="secondary"
+                      role={undefined}
+                      variant="contained"
+                      tabIndex={-1}
+                      startIcon={<CloudUploadIcon />}
+                    >
+                      Upload ID proof
+                      <VisuallyHiddenInput type="file" accept="image/*" onChange={handleIDChange} />
+                    </Button>
+                  </Box>
+                  {selectedID != null ? (
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                      <Typography
+                        variant="body1"
+                        sx={{ width: '100px', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}
+                      >
+                        {selectedID?.name}
+                      </Typography>
+                      <IconButton color="primary" onClick={handleDeleteFileID}>
+                        <Delete />
+                      </IconButton>
+                    </Box>
+                  ) : null}
+                </Box>
+              </Grid>
+              <Grid item xs={6}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">To Meet</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={template}
+                    label="Purpose"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value={1}>Admin</MenuItem>
+                    <MenuItem value={2}>Teacher</MenuItem>
+                    <MenuItem value={3}>Principal</MenuItem>
+                    <MenuItem value={4}>Peon</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <Paper>
+            <Grid container spacing={3} sx={{ p: '25px' }}>
+              <Grid item xs={12}>
+                <Box>
+                  <Box style={{ display: 'none' }}>
+                    <ComponentToPrint
+                      ref={componentRef}
+                      user={value}
+                      school={{
+                        name: 'SHAURYA INTERNATIONAL SCHOOL, GURUGRAM',
+                        address: 'Shree Awaas Apartment-Dwarka',
+                        phone: '011-45525909',
+                        other: 'other props mention here'
+                      }}
+                    />
+                  </Box>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    endIcon={<PrintIcon />}
+                    fullWidth
+                    sx={{ display: 'flex', justifyContent: 'center' }}
+                    onClick={handlePrint}
+                  >
+                    Print
+                  </Button>
+                </Box>
+              </Grid>
+              <Grid item xs={12}>
+                <Box>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    endIcon={<PeopleOutlineOutlinedIcon />}
+                    onClick={toggleDrawer('right', true)}
+                    fullWidth
+                  >
+                    Show Visitors
+                  </Button>
+                  <Drawer anchor={'right'} open={state['right']} onClose={toggleDrawer('right', false)}>
+                    <VisitorDrawer />
+                  </Drawer>
+                </Box>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default ScheduledVisit;
--- src/views/sidebar-menus/upload-photogallery/UploadGalleryDrawer/CreateNewDetails.js
+++ src/views/sidebar-menus/upload-photogallery/UploadGalleryDrawer/CreateNewDetails.js
@@ -10,8 +10,11 @@ import {
   MenuItem,
   FormControl,
   IconButton,
-  Box,Paper,
-  Checkbox,FormControlLabel, InputLabel,
+  Box,
+  Paper,
+  Checkbox,
+  FormControlLabel,
+  InputLabel
 } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import { Delete } from '@mui/icons-material';
@@ -20,16 +23,16 @@ const useStyles = makeStyles((theme) => ({
   formControl: {
     margin: theme.spacing(1),
     minWidth: 120,
-    width: '100%',
+    width: '100%'
   },
   uploadButton: {
-    marginTop: theme.spacing(2),
+    marginTop: theme.spacing(2)
   },
   fileContainer: {
     display: 'flex',
     alignItems: 'center',
-    marginBottom: theme.spacing(1),
-  },
+    marginBottom: theme.spacing(1)
+  }
 }));
 
 const steps = ['Enter Details', 'Upload Media', 'Review'];
@@ -38,7 +41,7 @@ const steps = ['Enter Details', 'Upload Media', 'Review'];
 const classToStudents = {
   'Class I': ['John', 'Alice', 'Bob'],
   'Class II': ['Sarah', 'David', 'Emma'],
-  'Class III': ['Michael', 'Olivia', 'William'],
+  'Class III': ['Michael', 'Olivia', 'William']
 };
 
 const CreateNewDetails = () => {
@@ -80,10 +83,7 @@ const CreateNewDetails = () => {
     } else if (selectedIndex === selectedStudents.length - 1) {
       newSelectedStudents = newSelectedStudents.concat(selectedStudents.slice(0, -1));
     } else if (selectedIndex > 0) {
-      newSelectedStudents = newSelectedStudents.concat(
-        selectedStudents.slice(0, selectedIndex),
-        selectedStudents.slice(selectedIndex + 1)
-      );
+      newSelectedStudents = newSelectedStudents.concat(selectedStudents.slice(0, selectedIndex), selectedStudents.slice(selectedIndex + 1));
     }
 
     setSelectedStudents(newSelectedStudents);
@@ -118,7 +118,7 @@ const CreateNewDetails = () => {
 
   return (
     <div>
-      <Stepper activeStep={activeStep} alternativeLabel  sx={{ paddingTop: '20px' }}>
+      <Stepper activeStep={activeStep} alternativeLabel sx={{ paddingTop: '20px' }}>
         {steps.map((label) => (
           <Step key={label}>
             <StepLabel>{label}</StepLabel>
@@ -128,7 +128,7 @@ const CreateNewDetails = () => {
       <div>
         {activeStep === 0 && (
           <div>
-             <Box sx={{ paddingLeft: '12px', paddingRight: '20px', paddingTop: '20px' }}>
+            <Box sx={{ paddingLeft: '12px', paddingRight: '20px', paddingTop: '20px' }}>
               <TextField
                 label="Album Name"
                 id="album-name"
@@ -196,7 +196,7 @@ const CreateNewDetails = () => {
                 </Paper>
               </Box>
             )}
-             <Box sx={{ paddingBottom: '12px', paddingLeft: '20px', paddingRight: '15px' }}>
+            <Box sx={{ paddingBottom: '12px', paddingLeft: '20px', paddingRight: '15px' }}>
               <TextField
                 label="Description"
                 id="description"
@@ -250,23 +250,23 @@ const CreateNewDetails = () => {
                     border: '2px dashed #ccc',
                     borderRadius: '8px',
                     padding: '20px',
-                    width: {xs:'95vw', sm:'600px'},
+                    width: { xs: '95vw', sm: '600px' },
                     height: '150px'
                   }}
                 >
                   <Typography variant="body1">Drag & Drop Files Here</Typography>
                   <Button
-                variant="outlined"
-                component="span"
-                className={classes.uploadButton}
-                onClick={() => document.getElementById('upload-media').click()}
-                style={{ marginTop: '35px' }}
-              >
-                Upload Files
-              </Button>
+                    variant="outlined"
+                    component="span"
+                    className={classes.uploadButton}
+                    onClick={() => document.getElementById('upload-media').click()}
+                    style={{ marginTop: '35px' }}
+                  >
+                    Upload Files
+                  </Button>
                 </Box>
               </label>
-             
+
               {selectedFiles.map((file, index) => (
                 <div key={index} className={classes.fileContainer}>
                   <Typography variant="body1">{file.name}</Typography>
@@ -280,56 +280,56 @@ const CreateNewDetails = () => {
         )}
         {activeStep === 2 && (
           <div>
-             <Box sx={{ padding: '20px' }}>
-             <Typography sx={{ padding: '20px' }}>Album Name: {albumName}</Typography>
+            <Box sx={{ padding: '20px' }}>
+              <Typography sx={{ padding: '20px' }}>Album Name: {albumName}</Typography>
               <Typography sx={{ padding: '20px' }}>Class: {className}</Typography>
               <Typography sx={{ padding: '20px' }}>Student Names: {selectedStudents.join(', ')}</Typography>
               <Typography sx={{ padding: '20px' }}>Description: {description}</Typography>
-            {selectedFiles.map((file, index) => (
-              <div key={index} className={classes.fileContainer}>
-                <Typography variant="body1">{file.name}</Typography>
-                <IconButton color="secondary" onClick={() => handleDeleteFile(index)}>
-                  <Delete />
-                </IconButton>
-              </div>
-            ))}
+              {selectedFiles.map((file, index) => (
+                <div key={index} className={classes.fileContainer}>
+                  <Typography variant="body1">{file.name}</Typography>
+                  <IconButton color="secondary" onClick={() => handleDeleteFile(index)}>
+                    <Delete />
+                  </IconButton>
+                </div>
+              ))}
             </Box>
           </div>
         )}
         <div>
           {activeStep === steps.length - 1 ? (
             <>
-            <Box sx={{ paddingTop: '20px', display: 'flex', justifyContent: 'end' }}>
-              <Button disabled={activeStep === 0} onClick={handleBack}>
-                Back
-              </Button>
-              <Button variant="contained" color="primary" onClick={handleSubmit}>
-                Post
-              </Button>
-            </Box>
-          </>
-        ) : (
-          <>
-            <Box sx={{ paddingTop: '20px', display: 'flex', justifyContent: 'end' }}>
-              <Button disabled={activeStep === 0} onClick={handleBack}>
-                Back
-              </Button>
-              {activeStep === 1 ? (
-                <Button variant="contained" color="primary" onClick={handleReview}>
-                  Review
+              <Box sx={{ paddingTop: '20px', display: 'flex', justifyContent: 'end' }}>
+                <Button disabled={activeStep === 0} onClick={handleBack}>
+                  Back
                 </Button>
-              ) : (
-                <Button variant="contained" color="primary" onClick={handleNext}>
-                  Next
+                <Button variant="contained" color="primary" onClick={handleSubmit}>
+                  Post
                 </Button>
-              )}
-            </Box>
-          </>
-        )}
+              </Box>
+            </>
+          ) : (
+            <>
+              <Box sx={{ paddingTop: '20px', display: 'flex', justifyContent: 'end' }}>
+                <Button disabled={activeStep === 0} onClick={handleBack}>
+                  Back
+                </Button>
+                {activeStep === 1 ? (
+                  <Button variant="contained" color="primary" onClick={handleReview}>
+                    Review
+                  </Button>
+                ) : (
+                  <Button variant="contained" color="primary" onClick={handleNext}>
+                    Next
+                  </Button>
+                )}
+              </Box>
+            </>
+          )}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
 };
 
 export default CreateNewDetails;
