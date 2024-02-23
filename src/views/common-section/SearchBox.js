cb049bf79ad52f0885619446b5ae418282d88f5f
Tarun Pandey <tarun@shauryasoft>
2024-02-23 17:40:11 +0530

    formStudentMaster

---
+++ src/views/common-section/SearchBox.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+
+const SearchBar = ({ 
+    placeholder = "Search...",
+    onChange,
+    onSearch,
+    inputProps = {},
+    buttonProps = {},
+    rootSx = {},
+    inputSx = { 
+    },
+    buttonSx = {}
+}) => {
+    const handleChange = (event) => {
+        if (onChange) {
+            onChange(event.target.value);
+        }
+    };
+
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter' && onSearch) {
+            onSearch();
+        }
+    };
+
+    return (
+        <Box sx={{ display: 'flex', alignItems: 'center', ...rootSx}}>
+            <InputBase
+                placeholder={placeholder}
+                onChange={handleChange}
+                onKeyPress={handleKeyPress}
+                sx={{ flex: 1, ...inputSx,border:'1px solid #a8adb1',borderRadius: '10px 0px 0px 10px', height: '50px',backgroundColor:'#f8fafc',borderRight:'none',paddingLeft:'20px'}}
+                inputProps={inputProps}
+            />
+            <IconButton onClick={onSearch} aria-label="search" sx={{...buttonSx,border:'1px solid #a8adb1',borderRadius: '0px 8px 8px 0px',height: '50px',backgroundColor:'#f8fafc',borderLeft:'none'}} {...buttonProps}>
+                <SearchIcon />
+            </IconButton>
+        </Box>
+    );
+};
+
+export default SearchBar;
--- src/views/erp-module/student-information-fee-management/student-info-fee-management-sidebarmenus/student-master/change-enrolment-number/index.js
+++ src/views/erp-module/student-information-fee-management/student-info-fee-management-sidebarmenus/student-master/change-enrolment-number/index.js
@@ -1,9 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Box, Paper, TextField, Drawer, Button } from '@mui/material';
+import ParameterizedAutoComplete from 'views/common-section/parameterizedAutoComplete';
+import DateComponent from 'views/common-section/DateComponent';
+import SearchBar from 'views/common-section/SearchBox';
+import RightDrawer from 'views/common-section/RightDrawer'
+
+const subjects = ["Delivered", "notdelivered", "yetto delivered"];
 
 const ChangeEnrolmentNumber = () => {
+  const [searchData, setsearchData] = useState({
+    "startDate": null
+  });
+  const [state, setState] = React.useState({
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setState({ ...state, [anchor]: open });
+  };
+
+  const handleSearchClick = () => {
+    const toggleRightDrawer = toggleDrawer('right', true);
+    toggleRightDrawer({ type: 'click' });
+  }
+
+  const handleDrawerClose = () => {
+    const toggleRightDrawer = toggleDrawer('right', false);
+    toggleRightDrawer({ type: 'click' });
+  }
+
+  const style = {
+    filter: {
+      width: "400px"
+    },
+    buttonSx: {
+      width: "50px"
+    },
+    inputSx: {
+      width: "350px"
+    }
+  }
+  const handleChange = (name, newValue) => {
+    setsearchData({ ...searchData, [name]: newValue });
+  }
+
+
   return (
-    <div>index</div>
+    <>
+      <Paper>
+        <Box container spacing={1} sx={{ padding: "20px 0px 20px 60px", display: "flex", flexWrap: "wrap", flexDirection: "row", gap: "40px" }}>
+          <ParameterizedAutoComplete option={subjects} label="Entrolement Type" onChange={(event, newData) => handleChange("loginName", newData)} customStyle={style.filter} />
+          <TextField id="outlined-basic" label="Admission No." variant="outlined" inputProps={{ maxLength: 10 }} sx={style.filter} onChange={(event) => handleChange("mobileNo", event.target.value)} />
+          <TextField id="outlined-basic" label="Date of Admission" variant="outlined" inputProps={{ maxLength: 10 }} sx={style.filter} onChange={(event) => handleChange("mobileNo", event.target.value)} />
+          <SearchBar placeholder='       Student Name' inputSx={style.inputSx} buttonSx={style.buttonSx} onSearch={handleSearchClick} />
+          <TextField id="outlined-basic" label="Class" variant="outlined" inputProps={{ maxLength: 10 }} sx={style.filter} onChange={(event) => handleChange("mobileNo", event.target.value)} />
+          <TextField id="outlined-basic" label="Contact Name" variant="outlined" inputProps={{ maxLength: 10 }} sx={style.filter} onChange={(event) => handleChange("mobileNo", event.target.value)} />
+          <ParameterizedAutoComplete option={subjects} label="Related Info" onChange={(event, newData) => handleChange("loginName", newData)} customStyle={style.filter} />
+          <DateComponent option={subjects} label="Left Reason" value={searchData?.startDate} onChange={(newDate) => handleChange("startDate", newDate)} customStyle={style.filter} />
+        </Box>
+        <Box>
+            <Button sx={{margin:"0px 0px 20px 60px",height: "40px", color: '#fff' }} variant="contained">Save</Button>
+            <Button sx={{margin:"0px 0px 20px 60px",height: "40px", color: '#fff'}} variant="contained">Cancel</Button>
+        </Box>
+        <Drawer
+          anchor="right"
+          open={state.right}
+        >
+          <RightDrawer onClose={handleDrawerClose} />
+        </Drawer>
+      </Paper>
+    </>
   )
 }
 
-export default ChangeEnrolmentNumber
+export default ChangeEnrolmentNumber;
