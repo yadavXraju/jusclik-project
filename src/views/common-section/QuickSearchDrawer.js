eaf55a835f7a7b7907abfb0c2e7dd9c937c7d041
Tarun Pandey <tarun@shauryasoft>
2024-02-24 12:24:40 +0530

    enrollement

--- src/views/common-section/QuickSearchDrawer.js
+++ src/views/common-section/QuickSearchDrawer.js
@@ -1,10 +1,21 @@
-import * as React from 'react';
+import React,{useState} from 'react';
 import { Box, Typography, Button, TextField } from '@mui/material';
 import ParameterizedAutoComplete from 'views/common-section/parameterizedAutoComplete';
 const subjects = ["Delivered", "notdelivered", "yetto delivered"];
 
 
-const RightDrawer = ({onClose}) => {
+const QuickSearchDrawer= ({onClose}) => {
+    const [quickSearch,setQuickSearch]=useState({
+        "student":"",
+        "left":"",
+        "class":"",
+        "section":""
+    });
+
+    const handleChange=async(name,newData)=>{
+       await setQuickSearch({...quickSearch,[quickSearch]:newData})
+    }
+
     const isMobile = window.innerWidth < 900;
     const style = {
         filter: {
@@ -23,7 +34,7 @@ const RightDrawer = ({onClose}) => {
             <Box container spacing={1} sx={{ padding: "20px 0px 20px 20px", display: "flex", flexWrap: "wrap", flexDirection: "row", gap: "20px" }}>
                 <TextField id="outlined-basic" label="Search Student name from" variant="outlined" inputProps={{ maxLength: 10 }} sx={style.filter} onChange={(event) => handleChange("student", event.target.value)} />
                 <ParameterizedAutoComplete option={subjects} label="Anywhere" onChange={(event, newData) => handleChange("left", newData)} customStyle={style.filter} />
-                <ParameterizedAutoComplete option={subjects} label="Class" onChange={(event, newData) => handleChange("kg", newData)} customStyle={style.filter} />
+                <ParameterizedAutoComplete option={subjects} label="Class" onChange={(event, newData) => handleChange("class", newData)} customStyle={style.filter} />
                 <ParameterizedAutoComplete option={subjects} label="Section" onChange={(event, newData) => handleChange("section", newData)} customStyle={style.filter} />
                 <Button sx={{ height: "40px", color: '#fff', margin: "auto 0px auto 0px" }} variant="contained">
                     Show
@@ -33,4 +44,4 @@ const RightDrawer = ({onClose}) => {
     );
 }
 
-export default RightDrawer;
+export default QuickSearchDrawer;
