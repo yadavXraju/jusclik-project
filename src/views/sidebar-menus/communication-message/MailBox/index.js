ffac9d200f3926b53c760db28a598e781b20d97c
Abhishek <abhishek@shauryasoft.com>
2024-02-27 16:29:40 +0530

    mail box path setting

--- src/views/sidebar-menus/communication-message/MailBox/index.js
+++ src/views/sidebar-menus/communication-message/MailBox/index.js
@@ -6,30 +6,13 @@ import { styled } from '@mui/system';
 import { useNavigate, useLocation } from 'react-router';
 import AvtarImg from '../../../../assets/images/avatar.png'
 import InboxHeader from './InboxHeader';
+import initialData from './InboxNameList';
 
 
 
 const theme = createTheme();
 
-// Sample data for initial inbox messages
-export const initialData = [
-  { id: 1, name: 'John Doe', role: 'Website Developer', message: 'Lorem ipsum dolor sit amet 1', unread: false, important: true },
-  { id: 2, name: 'Jane Smith', role: 'Software Developer', message: 'Lorem ipsum dolor sit amet 2', unread: true, important: false },
-  { id: 3, name: 'Bob Johnson', role: 'Front-end Developer', message: 'Lorem ipsum dolor sit amet 3', unread: false, important: true },
-  { id: 4, name: 'Alice Williams', role: 'Developer', message: 'Lorem ipsum dolor sit amet 4', unread: true, important: false },
-  { id: 5, name: 'Charlie Brown', role: 'Manager', message: 'Lorem ipsum dolor sit amet 5', unread: false, important: true },
-  { id: 6, name: 'Eva Davis', role: 'CEO', message: 'Lorem ipsum dolor sit amet 6', unread: false, important: false },
-  { id: 7, name: 'Frank White', role: 'Managing Director', message: 'Lorem ipsum dolor sit amet 7', unread: true, important: true },
-  { id: 8, name: 'Grace Miller', role: 'Businessman', message: 'Lorem ipsum dolor sit amet 8', unread: false, important: false },
-  { id: 9, name: 'Henry Jackson', role: 'Developer', message: 'Lorem ipsum dolor sit amet 9', unread: true, important: true },
-  { id: 10, name: 'Ivy Lee', role: 'Developer', message: 'Lorem ipsum dolor sit amet 10', unread: false, important: false },
-  { id: 11, name: 'Charlie Brown', role: 'Developer', message: 'Lorem ipsum dolor sit amet 5', unread: false, important: true },
-  { id: 12, name: 'Eva Davis', role: 'Developer', message: 'Lorem ipsum dolor sit amet 6', unread: false, important: false },
-  { id: 13, name: 'Frank White', role: 'Developer', message: 'Lorem ipsum dolor sit amet 7', unread: true, important: true },
-  { id: 14, name: 'Grace Miller', role: 'Developer', message: 'Lorem ipsum dolor sit amet 8', unread: false, important: false },
-  { id: 15, name: 'Henry Jackson', role: 'Developer', message: 'Lorem ipsum dolor sit amet 9', unread: true, important: true },
-  { id: 16, name: 'Ivy Lee', role: 'Developer', message: 'Lorem ipsum dolor sit amet 10', unread: false, important: false },
-];
+
 
 const GmailInboxTemplate = () => {
   const searchInputRef = useRef(null);
