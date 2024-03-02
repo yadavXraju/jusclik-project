933e800b1d71f2921eeb6a17c859d9c04a53a794
vikash <vikash@shauryasoft.com>
2024-03-02 10:00:49 +0530

    Drawer update

--- src/views/sidebar-menus/leave-application/LeaveApplicationDrawer.js
+++ src/views/sidebar-menus/leave-application/LeaveApplicationDrawer.js
@@ -1,33 +1,52 @@
-import * as React from 'react';
-import Drawer from '@mui/material/Drawer';
-import useDrawer from 'hooks/useDrawer';
-import Button from '@mui/material/Button';
-import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
-import { Box } from '@mui/system';
+// import * as React from 'react';
+// import Drawer from '@mui/material/Drawer';
+// import useDrawer from 'hooks/useDrawer';
+// import Button from '@mui/material/Button';
+// import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+// import { Box } from '@mui/system';
+// import LeaveApplicationDrawerContent from './LeaveApplicationDrawerContent';
+// import { Typography } from '@mui/material';
+
+
+// export default function LeaveDrawer() {
+//   const { anchor, toggleDrawer } = useDrawer();
+
+//   return (
+//     <div>
+//       <Button onClick={toggleDrawer('right', true)} variant="outlined" startIcon={<AddOutlinedIcon />}>
+//         Leave Application
+//       </Button>
+//       <Drawer anchor={'right'} open={anchor.right} onClose={toggleDrawer('right', false)}>
+//       <Box sx={{ width: { xs: '100%', sm: 650 }, padding: 2 }} role="presentation">
+//         <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', borderBottom: '1px solid #ccc' }}>
+//           <Typography variant="h4">Request Leave</Typography>
+//           <Button onClick={toggleDrawer('right', false)} sx={{ alignSelf: 'flex-end' }}>
+//             Close
+//           </Button>
+//         </Box>
+//         <LeaveApplicationDrawerContent/>
+//       </Box>
+        
+//       </Drawer>
+//     </div>
+//   );
+// }
+
+
+
+
+
+import React from 'react'
+import withParamDrawer from 'views/common-section/withParamDrawer';
 import LeaveApplicationDrawerContent from './LeaveApplicationDrawerContent';
-import { Typography } from '@mui/material';
 
-
-export default function LeaveDrawer() {
-  const { anchor, toggleDrawer } = useDrawer();
-
+const NewParamDrawer=withParamDrawer(LeaveApplicationDrawerContent);
+function ExpensesDrawer() {
   return (
     <div>
-      <Button onClick={toggleDrawer('right', true)} variant="outlined" startIcon={<AddOutlinedIcon />}>
-        Leave Application
-      </Button>
-      <Drawer anchor={'right'} open={anchor.right} onClose={toggleDrawer('right', false)}>
-      <Box sx={{ width: { xs: '100%', sm: 650 }, padding: 2 }} role="presentation">
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', borderBottom: '1px solid #ccc' }}>
-          <Typography variant="h4">Request Leave</Typography>
-          <Button onClick={toggleDrawer('right', false)} sx={{ alignSelf: 'flex-end' }}>
-            Close
-          </Button>
-        </Box>
-        <LeaveApplicationDrawerContent/>
-      </Box>
-        
-      </Drawer>
+      <NewParamDrawer buttonLabel='Leave Application' drawerTitle='Request Leave' />
     </div>
-  );
+  )
 }
+
+export default ExpensesDrawer
