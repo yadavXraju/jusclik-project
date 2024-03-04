53270a91aec71501d4f961ebe68b9a88ce5b20ec
vikash <vikash@shauryasoft.com>
2024-03-04 11:27:01 +0530

    setup

--- src/views/erp-module/erp-common-item/setup/system-data/index.js
+++ src/views/erp-module/erp-common-item/setup/system-data/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import SetupTabs from '../Setup'
+import SetupTabs from './Setup'
+import { Card } from '@mui/material'
 
 const SystemData = () => {
   return (
     <>
+    <Card padding={1}>
     <SetupTabs/>
+    </Card>
     </>
   )
 }
 
-export default SystemData
+export default SystemData 
