2f9e97f4c4e4937dbe061d42963eca81a68bd723
vikash <vikash@shauryasoft.com>
2024-03-07 17:22:40 +0530

    Role & user

--- src/views/erp-module/student-information-fee-management/student-info-fee-management-sidebarmenus/user-and-roles/RoleAccess.js
+++ src/views/erp-module/student-information-fee-management/student-info-fee-management-sidebarmenus/user-and-roles/RoleAccess.js
@@ -7,100 +7,117 @@ import TableCell from '@mui/material/TableCell';
 import { Checkbox, Box, Card, Typography } from '@mui/material';
 
 function RoleAccess() {
-    const [moduleAccess, setModuleAccess] = useState({});
+  const [moduleAccess, setModuleAccess] = useState({});
 
-    const modulesData = [
-        { id: '1', details: { moduleName: 'Stufee' } },
-        { id: '2', details: { moduleName: 'Payroll' } },
-        { id: '3', details: { moduleName: 'Progress' } },
-        { id: '4', details: { moduleName: 'Library' } },
-        { id: '5', details: { moduleName: 'Inventory' } },
-        { id: '6', details: { moduleName: 'Website' } },
-        { id: '7', details: { moduleName: 'Visitor Mgmt' } },
-        { id: '8', details: { moduleName: 'Medical' } },
-    ];
+  const modulesData = [
+    { id: '1', details: { moduleName: 'Stufee', chacked:'false' } },
+    { id: '2', details: { moduleName: 'Payroll', chacked:'true' } },
+    { id: '3', details: { moduleName: 'Progress', chacked:'false' } },
+    { id: '4', details: { moduleName: 'Library', chacked:'true' } },
+    { id: '5', details: { moduleName: 'Inventory', chacked:'false' } },
+    { id: '6', details: { moduleName: 'Website', chacked:'true' } },
+    { id: '7', details: { moduleName: 'Visitor Mgmt', chacked:'false' } },
+    { id: '8', details: { moduleName: 'Medical', chacked:'true' } }
+  ];
 
-    const handleFullAccessChange = (moduleId, checked) => {
-        const updatedModuleAccess = { ...moduleAccess };
-        updatedModuleAccess[moduleId] = {
-            fullAccess: checked,
-            view: checked,
-            create: checked,
-            edit: checked,
-            approve: checked,
-        };
-        setModuleAccess(updatedModuleAccess);
+  const handleFullAccessChange = (moduleId, checked) => {
+    const updatedModuleAccess = { ...moduleAccess };
+    updatedModuleAccess[moduleId] = {
+      fullAccess: checked,
+      view: checked,
+      create: checked,
+      edit: checked,
+      delete: checked,
+      approve: checked
     };
+    setModuleAccess(updatedModuleAccess);
+  };
 
-    const handleCheckboxChange = (moduleId, key, checked) => {
-        const updatedModuleAccess = { ...moduleAccess };
-        updatedModuleAccess[moduleId] = { ...updatedModuleAccess[moduleId], [key]: checked };
-        setModuleAccess(updatedModuleAccess);
-    };
+  const handleCheckboxChange = (moduleId, key, checked) => {
+    const updatedModuleAccess = { ...moduleAccess };
+    updatedModuleAccess[moduleId] = { ...updatedModuleAccess[moduleId], [key]: checked };
+    setModuleAccess(updatedModuleAccess);
+  };
 
-    return (
-        <Card sx={{ width: { xs: '83vw', md: '100%' }, overflow: { xs: 'scroll', md: 'hidden' }, border: '1px solid #ccc', borderRadius: '5px', padding: '10px' }}>
-            <Box p={2} sx={{borderBottom:'1px solid #ccc'}}>
-                <Typography variant='h4'>
-                    Modules Access
-                </Typography>
-            </Box>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell></TableCell>
-                        <TableCell>Full Access</TableCell>
-                        <TableCell>View</TableCell>
-                        <TableCell>Create</TableCell>
-                        <TableCell>Edit</TableCell>
-                        <TableCell>Approve</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {modulesData.map(module => (
-                        <TableRow key={module.id}>
-                            <TableCell sx={{fontWeight:'bold'}}>{module.details.moduleName}</TableCell>
-                            <TableCell style={{ paddingLeft: '14px' }}>
-                                <Checkbox
-                                    size='small'
-                                    checked={moduleAccess[module.id]?.fullAccess || false}
-                                    onChange={(e) => handleFullAccessChange(module.id, e.target.checked)}
-                                />
-                            </TableCell>
-                            <TableCell>
-                                <Checkbox
-                                    size='small'
-                                    checked={moduleAccess[module.id]?.view || false}
-                                    onChange={(e) => handleCheckboxChange(module.id, 'view', e.target.checked)}
-                                />
-                            </TableCell>
-                            <TableCell>
-                                <Checkbox
-                                    size='small'
-                                    checked={moduleAccess[module.id]?.create || false}
-                                    onChange={(e) => handleCheckboxChange(module.id, 'create', e.target.checked)}
-                                />
-                            </TableCell>
-                            <TableCell>
-                                <Checkbox
-                                    size='small'
-                                    checked={moduleAccess[module.id]?.edit || false}
-                                    onChange={(e) => handleCheckboxChange(module.id, 'edit', e.target.checked)}
-                                />
-                            </TableCell>
-                            <TableCell>
-                                <Checkbox
-                                    size='small'
-                                    checked={moduleAccess[module.id]?.approve || false}
-                                    onChange={(e) => handleCheckboxChange(module.id, 'approve', e.target.checked)}
-                                />
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </Card>
-    );
+  return (
+    <>
+      <Box p={2}>
+        <Typography variant="h4">Modules Access</Typography>
+      </Box>
+      <Card
+        sx={{
+          width: { xs: '83vw', md: '100%' },
+          overflow: { xs: 'scroll', md: 'hidden' },
+          border: '1px solid #ccc',
+          borderRadius: '5px',
+          padding: '10px'
+        }}
+      >
+        <Table>
+          <TableHead>
+            <TableRow sx={{background:'#f0f8ff'}}>
+              <TableCell></TableCell>
+              <TableCell sx={{fontWeight:'bold'}}>Full Access</TableCell>
+              <TableCell sx={{fontWeight:'bold'}}>View</TableCell>
+              <TableCell sx={{fontWeight:'bold'}}>Create</TableCell>
+              <TableCell sx={{fontWeight:'bold'}}>Edit</TableCell>
+              <TableCell sx={{fontWeight:'bold'}}>Delete</TableCell>
+              <TableCell sx={{fontWeight:'bold'}}>Approve</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {modulesData.map((module) => (
+              <TableRow key={module.id}>
+                <TableCell sx={{ fontWeight: 'bold', padding: '10px' }}>{module.details.moduleName}</TableCell>
+                <TableCell style={{ paddingLeft: '14px', padding: '10px' }}>
+                  <Checkbox
+                    size="small"
+                    checked={moduleAccess[module.id]?.fullAccess || false}
+                    onChange={(e) => handleFullAccessChange(module.id, e.target.checked)}
+                  />
+                </TableCell>
+                <TableCell style={{ padding: '10px' }}>
+                  <Checkbox
+                    size="small"
+                    checked={moduleAccess[module.id]?.view || false}
+                    onChange={(e) => handleCheckboxChange(module.id, 'view', e.target.checked)}
+                  />
+                </TableCell>
+                <TableCell style={{ padding: '10px' }}>
+                  <Checkbox
+                    size="small"
+                    checked={moduleAccess[module.id]?.create || false}
+                    onChange={(e) => handleCheckboxChange(module.id, 'create', e.target.checked)}
+                  />
+                </TableCell>
+                <TableCell style={{ padding: '10px' }}>
+                  <Checkbox
+                    size="small"
+                    checked={moduleAccess[module.id]?.edit || false}
+                    onChange={(e) => handleCheckboxChange(module.id, 'edit', e.target.checked)}
+                  />
+                </TableCell>
+                <TableCell style={{ padding: '10px' }}>
+                  <Checkbox
+                    size="small"
+                    checked={moduleAccess[module.id]?.delete || false}
+                    onChange={(e) => handleCheckboxChange(module.id, 'delete', e.target.checked)}
+                  />
+                </TableCell>
+                <TableCell style={{ padding: '10px' }}>
+                  <Checkbox
+                    size="small"
+                    checked={moduleAccess[module.id]?.approve || false}
+                    onChange={(e) => handleCheckboxChange(module.id, 'approve', e.target.checked)}
+                  />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Card>
+    </>
+  );
 }
 
 export default RoleAccess;
