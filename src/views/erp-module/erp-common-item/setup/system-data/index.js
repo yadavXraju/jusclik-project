8725348da72fad22920913caa993d067cfc086bc
Tarun Pandey <tarun@shauryasoft>
2024-03-09 15:22:46 +0530

    error fixed

--- src/views/erp-module/erp-common-item/setup/system-data/index.js
+++ src/views/erp-module/erp-common-item/setup/system-data/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import PropTypes from 'prop-types';
 import { Tabs, Tab, Typography, Box, Button } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
