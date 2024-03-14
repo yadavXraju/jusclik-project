8e2b8a854bddb66a2a415354788a924640e2f3d8
Damandeep <damandeep@shauryasoft>
2024-03-14 17:52:59 +0530

    report layout modify

--- src/views/erp-module/student-information-fee-management/student-info-fee-management-sidebarmenus/reports/report-layout.js
+++ src/views/erp-module/student-information-fee-management/student-info-fee-management-sidebarmenus/reports/report-layout.js
@@ -4,7 +4,6 @@ import {
     RadioGroup,
     TextField,
     Typography,
-    Paper,
     FormControl,
     Select,
     MenuItem,
@@ -15,6 +14,7 @@ import {
 } from '@mui/material'
 import { Box } from '@mui/system';
 import { useState } from 'react';
+import ReportPreview from './reportPreview';
 
 const ReportLayout = () => {
 
@@ -38,13 +38,23 @@ const ReportLayout = () => {
             justifyContent: 'center',
             alignItems: 'center',
             display: 'flex'
+        },
+        inputBox:{
+            paddingBottom:'24px',
+            paddingRight:'24px'
+        },
+        fontFamilyBox:{
+            padding:'30px 0'
+        },
+        fontFamilyText:{
+            paddingTop:'24px'
         }
     }
 
     // form state===========================================================
     const [formState, setFormState] = useState({
-        tableDensity: '',
-        tableDesign: '',
+        tableDensity: 'classic',
+        tableDesign: 'default',
         resizeTable: false,
         organizationName: false,
         reportBasis: false,
@@ -52,13 +62,13 @@ const ReportLayout = () => {
         generatedBy: false,
         generatedDate: false,
         generatedTime: false,
-        paperSizeA4: true,
-        pageOrientationPortrait: true,
-        pageFontfamily: '',
-        pageMarginTop: '',
-        pageMarginBottom: '',
-        pageMarginLeft: '',
-        pageMarginRight: '',
+        pageSize: 'a4',
+        pageOrientation: 'portrait',
+        pageFontfamily: 'jakarta',
+        pageMarginTop: '0.7',
+        pageMarginBottom: '0.7',
+        pageMarginLeft: '0.5',
+        pageMarginRight: '0.2',
     })
 
     // handler function===========================================================================
@@ -72,19 +82,22 @@ const ReportLayout = () => {
     }
 
 
-    console.log(formState);
     return (<>
-        <Box>
-{/* left side components =======================================================*/}
+
+    <Grid container>
+        <Grid item md={5}>
+            {/* for left side components */}
+            <Box>
+            {/* left side components =======================================================*/}
             {/* form -------------------------------------------------------------------------------*/}
             <FormControl>
                 <Grid container spacing={6}>
                     {/* 2 dropdowns -------------------------------------------------------------------*/}
-                    <Grid item md={3}>
+                    <Grid item md={4}>
                         <FormControl fullWidth>
                             <InputLabel>Table Density</InputLabel>
                             <Select
-                                variant='outlined'
+                                label='Table Density'
                                 onChange={(e) => handleState('tableDensity', e.target.value)}>
                                 <MenuItem value={'classic'}>Classic</MenuItem>
                                 <MenuItem value={'compact'}>Compact</MenuItem>
@@ -92,11 +105,11 @@ const ReportLayout = () => {
                             </Select>
                         </FormControl>
                     </Grid>
-                    <Grid item md={3}>
+                    <Grid item md={4}>
                         <FormControl fullWidth>
                             <InputLabel>Table Design</InputLabel>
                             <Select
-                                variant='outlined'
+                                label='Table Design'
                                 onChange={(e) => handleState('tableDesign', e.target.value)}
                             >
                                 <MenuItem value={'default'}>Default</MenuItem>
@@ -107,23 +120,23 @@ const ReportLayout = () => {
                         </FormControl>
                     </Grid>
                 </Grid>
-                <FormControlLabel control={<Checkbox checked={formState.resizeTable} onChange={(e)=>(handleState('resizeTable',e.target.checked))} />} label={'Re-size the table and its font automatically to fit the content within the table.'} />
+                <FormControlLabel control={<Checkbox checked={formState.resizeTable} onChange={(e) => (handleState('resizeTable', e.target.checked))} />} label={'Re-size the table and its font automatically to fit the content within the table.'} />
                 {/* seperator ----------------------------------------------------------------*/}
                 <Box sx={css.seperator}></Box>
                 {/* typography -----------------------------------------------------------*/}
                 <Typography sx={css.heading.fontsize}>Choose Details to Display</Typography>
                 {/* 3 checkboxes in  2 rows  ------------------------------------------*/}
                 {/*ROW 1 ------------------------------------------------------------  */}
-                <Grid container spacing={4}>
-                    <Grid item>
+                <Grid container >
+                    <Grid item md={6}>
                         <Grid item>
-                            <FormControlLabel control={<Checkbox checked={formState.organizationName} onChange={(e)=>(handleState('organizationName',e.target.checked))} />} label={'Organization Name'} />
+                            <FormControlLabel control={<Checkbox checked={formState.organizationName} onChange={(e) => (handleState('organizationName', e.target.checked))} />} label={'Organization Name'} />
                         </Grid>
                         <Grid item>
-                            <FormControlLabel control={<Checkbox checked={formState.reportBasis} onChange={(e)=>(handleState('reportBaasis',e.target.checked))}/>} label={'Report Basis'} />
+                            <FormControlLabel control={<Checkbox checked={formState.reportBasis} onChange={(e) => (handleState('reportBasis', e.target.checked))} />} label={'Report Basis'} />
                         </Grid>
                         <Grid item>
-                            <FormControlLabel control={<Checkbox checked={formState.pageNumber} onChange={(e)=>(handleState('pageNumber',e.target.checked))}/>} label={'Page Number'} />
+                            <FormControlLabel control={<Checkbox checked={formState.pageNumber} onChange={(e) => (handleState('pageNumber', e.target.checked))} />} label={'Page Number'} />
                         </Grid>
                     </Grid>
                     {/* </Grid> */}
@@ -132,13 +145,13 @@ const ReportLayout = () => {
                     <Grid item>
 
                         <Grid item>
-                            <FormControlLabel control={<Checkbox checked={formState.generatedBy} onChange={(e)=>(handleState('generatedBy',e.target.checked))}/>} label={'Generated By'} />
+                            <FormControlLabel control={<Checkbox checked={formState.generatedBy} onChange={(e) => (handleState('generatedBy', e.target.checked))} />} label={'Generated By'} />
                         </Grid>
                         <Grid item>
-                            <FormControlLabel control={<Checkbox checked={formState.generatedDate} onChange={(e)=>(handleState('generatedDate',e.target.checked))}/>} label={'Generated Date'} />
+                            <FormControlLabel control={<Checkbox checked={formState.generatedDate} onChange={(e) => (handleState('generatedDate', e.target.checked))} />} label={'Generated Date'} />
                         </Grid>
                         <Grid item>
-                            <FormControlLabel control={<Checkbox checked={formState.generatedTime} onChange={(e)=>(handleState('generatedTime',e.target.checked))}/>} label={'Generated Time'} />
+                            <FormControlLabel control={<Checkbox checked={formState.generatedTime} onChange={(e) => (handleState('generatedTime', e.target.checked))} />} label={'Generated Time'} />
                         </Grid>
                     </Grid>
                 </Grid>
@@ -147,7 +160,7 @@ const ReportLayout = () => {
                 {/* {/* 2 coloumn 2 row  */}
                 <Grid container>
                     {/* column1========================================================================= */}
-                    <Grid item>
+                    <Grid item md={6}>
                         {/* coloumn 1 row1: 2*radio button */}
                         {/* <Typography sx={css.heading} >Paper Size</Typography> */}
                         <Grid item>
@@ -155,75 +168,97 @@ const ReportLayout = () => {
                                 <Typography sx={css.heading.fontsize}>Paper Size</Typography>
                                 <RadioGroup
                                     aria-labelledby=""
-                                    defaultValue="A4"
+                                    defaultValue="a4"
                                     name=""
+                                    onChange={(e) => (handleState('pageSize', e.target.value))}
                                 >
                                     <Grid container>
-                                        <FormControlLabel value="A4" control={<Radio />} label="A4" />
-                                        <FormControlLabel value="Letter" control={<Radio />} label="Letter" />
+                                        <FormControlLabel value="a4" control={<Radio />} label="A4" />
+                                        <FormControlLabel value="letter" control={<Radio />} label="Letter" />
                                     </Grid>
                                 </RadioGroup>
                             </FormControl>
                         </Grid>
                         {/* column1 row2 : dropdown */}
-                        <Grid item md={3}>
-                            <Box sx={css.heading.fontsize}></Box>
+                        <Grid item md={6}>
+                            <Box sx={css.fontFamilyBox}></Box>
                             <FormControl fullWidth>
                                 <InputLabel>Font Family</InputLabel>
                                 <Select
-                                    onChange={() => { }}
+                                    label='Font Family'
+                                    onChange={(e) => (handleState('pageFontFamily', e.target.value))}
                                 >
-                                    <MenuItem value={10}>Roboto</MenuItem>
-                                    <MenuItem value={20}>Poppins</MenuItem>
-                                    <MenuItem value={30}>Jakarta</MenuItem>
+                                    <MenuItem value={'roboto'}>Roboto</MenuItem>
+                                    <MenuItem value={'poppins'}>Poppins</MenuItem>
+                                    <MenuItem value={'jakarata'}>Jakarta</MenuItem>
                                 </Select>
                             </FormControl>
                         </Grid>
-                        <Grid item md={6}>
-
-                            <Typography>Supports English and European languages. This font can also render Indian Rupees Symbol.</Typography>
+                        <Grid item md={8}>
+                            
+                            <Typography sx={css.fontFamilyText}>Supports English and European languages. This font can also render Indian Rupees Symbol.</Typography>
                         </Grid>
                     </Grid>
                     {/* COLUMN 2======================================================================================== */}
+                    <Grid item md={6}>
+
+                    
                     <Grid item>
                         {/*  column 2 row1 :2*radio button */}
                         <FormControl>
                             <Typography sx={css.heading.fontsize}>Orientation</Typography>
                             <RadioGroup
                                 aria-labelledby=""
-                                defaultValue="Portrait"
+                                defaultValue="portrait"
                                 name=""
+                                onChange={(e) => (handleState('pageOrientation', e.target.value))}
                             >
                                 <Grid container>
-                                    <FormControlLabel value="Portrait" control={<Radio />} label="Portrait" />
-                                    <FormControlLabel value="Landscape" control={<Radio />} label="Landscape" />
+                                    <FormControlLabel value="portrait" control={<Radio />} label="Portrait" />
+                                    <FormControlLabel value="pandscape" control={<Radio />} label="Landscape" />
                                 </Grid>
                             </RadioGroup>
                         </FormControl>
-
+                        </Grid>
                         {/*  column2 row2: 4*input feild---------------------------------*/}
+                        <Grid item>
+                        <FormControl>
                         <Typography sx={css.heading.fontsize}>Margins</Typography>
-                        <Grid container spacing={2}>
-                            <Grid item md={12}>
-                                <TextField id="" label="Top" variant="outlined" />
-                                <TextField id="" label="Bottom" variant="outlined" />
+                        <Grid container >
+                            <Grid item md={6}>
+                                <TextField id="" label="Top" sx={css.inputBox} onChange={(e)=>(handleState('pageMarginTop',e.target.value))} />
                             </Grid>
-                            <Grid item md={12}>
-                                <TextField id="" label="Left" variant="outlined" />
-                                <TextField id="" label="Right" variant="outlined" />
+                            <Grid item md={6}>
+                                <TextField id="" label="Bottom" sx={css.inputBox} onChange={(e)=>(handleState('pageMarginBottom',e.target.value))} />
                             </Grid>
+                            <Grid item md={6}>
+                                <TextField id="" label="Left" sx={css.inputBox} onChange={(e)=>(handleState('pageMarginLeft',e.target.value))} />
+                            </Grid>
+                            <Grid item md={6}>
+                                <TextField id="" label="Right" sx={css.inputBox} onChange={(e)=>(handleState('pageMarginRight',e.target.value))} />
+                            </Grid>
+                            
+    
+                        </Grid>
+                        </FormControl>
                         </Grid>
                     </Grid>
+                    
                 </Grid>
             </FormControl>
-{/* right side components ----------------------------------------------------------------------*/}
-            {/* box  ---------------------------------------------------------------------*/}
-            <Box>
-                {/* paper  ---------------------------------------------------------------------*/}
-                <Paper></Paper>
-                {/* typography  ---------------------------------------------------------------------*/}
-                {/* table  ---------------------------------------------------------------------*/}
             </Box>
+        </Grid>
+        <Grid item md={1}></Grid>
+        <Grid item md={3}> 
+        {/* for right side preview component */}
+        <ReportPreview formState={formState} />
+        </Grid>
+    </Grid>
+      
+            {/* right side component ----------------------------------------------------------------------*/}
+            
+            <Box>
+               
         </Box>
     </>
     );
