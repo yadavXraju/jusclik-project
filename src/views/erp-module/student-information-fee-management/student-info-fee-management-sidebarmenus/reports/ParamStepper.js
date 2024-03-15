92f0c8cb5c85d264f12dd413c57c24e5bea59cc4
Tarun Pandey <tarun@shauryasoft>
2024-03-15 18:03:29 +0530

    reports

--- src/views/erp-module/student-information-fee-management/student-info-fee-management-sidebarmenus/reports/ParamStepper.js
+++ src/views/erp-module/student-information-fee-management/student-info-fee-management-sidebarmenus/reports/ParamStepper.js
@@ -1,24 +1,25 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Tabs, Tab, Typography, Box, Paper, Button } from '@mui/material';
+import { Tabs, Tab, Typography, Box} from '@mui/material';
 import ChevronRightOutlinedIcon from '@mui/icons-material/ChevronRightOutlined';
+import BottomNavbar from 'views/common-section/BottomNavbar';
 
 const TabsName = ({ name, number }) => {
   return (
     <Box sx={{ display: 'flex', gap: '8px', alignItems: 'center', justifyContent: 'space-between' }}>
       <Box sx={{ display: 'flex', gap: '5px', alignItems: "center" }}>
         <span style={{
-          display: 'inline-block',
-          width: '30px',
-          height: '30px',
-          borderRadius: '50%',
-          color: '#344040',
-          textAlign: 'center',
-          lineHeight: '25px',
-          fontSize: '16px',
-          fontWeight: 'bold',
-          border: '1px solid #ddd',
-          boxSizing: 'border-box',
+         display: 'inline-block',
+         width: '23px',
+         height: '23px',
+         borderRadius: '50%',
+         color:'rgb(52, 64, 64)',
+         textAlign: 'center',
+         lineHeight: '22px',
+         fontSize: '15px',
+         fontWeight: 'bold',
+         border: '1px solid rgb(221, 221, 221)',
+         boxSizing: 'border-box',
         }}>{number + 1}</span>
         <Typography>{name}</Typography>
       </Box>
@@ -61,31 +62,26 @@ const a11yProps = (index) => {
 
 const SetupTabs = ({ tabPage }) => {
   const [value, setValue] = useState(0);
-  const handleChange = (newValue) => {
+
+  const handleChange = (event,newValue) => {
+    console.log(newValue);
     setValue(newValue);
-  };
-
-  const handlePrev = () => {
-    handleChange(Math.max(0, value - 1));
-  };
-
-  const handleNext = () => {
-    handleChange(Math.min(tabPage.length - 1, value + 1));
   };
 
   return (
     <>
-      <Box sx={{ width: "100%", display: 'flex', flexDirection: "column", marginTop: "25px" }}>
+      <Box sx={{width: "100%",display: 'flex',flexDirection: "column",marginTop: "25px" }}>
         <Tabs
           orientation="horizontal"
           value={value}
           onChange={handleChange}
           aria-label="Vertical tabs example"
-          sx={{ borderBottom: "1px solid #eef2f6", width: "1000px", bgcolor: 'background.paper', display: "flex" }}
+          sx={{alignItems:"flex-start", borderBottom: "1px solid #eef2f6", bgcolor: 'background.paper', display: "flex" }}
         >
           {tabPage.map((tab, index) => (
             <Tab
               key={tab?.id}
+            
               label={<TabsName name={tab.name} number={index} />}
               {...a11yProps(index)}
               sx={{ width: "250px", borderBottom: "1px solid #eef2f6", alignItems: "baseline" }}
@@ -97,25 +93,13 @@ const SetupTabs = ({ tabPage }) => {
             key={tab?.id}
             value={value}
             index={index}
-            sx={{ width: "100%", bgcolor: 'background.paper', borderRadius: "10px", overflowX: "auto" }}
+            sx={{width: "100%",padding:"20px 20px 20px 20px",height:"600px",bgcolor: 'background.paper', borderRadius: "10px", overflowY: "auto" }}
           >
             <tab.component />
           </TabPanel>
         ))}
-        <Paper sx={{marginLeft:"-5px",alginItems: "center", display: "flex", width: "100%", gap: "20px", backgroundColor: "#fafafa", height: "60px", borderBottom: '1px solid #eee', borderTop: '1px solid #eee', position: "fixed", bottom: "90px" }}>
-          <Box sx={{display: "flex",gap:"20px",marginLeft: "5%"}}>
-            <Button variant="contained" sx={{ height: "38px",marginTop: "10px", width: "100px" }} onClick={handlePrev}>
-              Previous
-            </Button>
-            <Button variant="contained" sx={{ height: "38px", marginTop: "10px", width: "100px" }} onClick={handleNext}>
-              Next
-            </Button>
-          </Box>
-          <Button variant="outlined" sx={{ height: "38px", marginTop: "10px", width: "100px" }}>
-            Cancel
-          </Button>
-        </Paper>
       </Box >
+      <BottomNavbar tabPageLength={tabPage.length}  value={value} setValue={setValue}/>
     </>
   );
 };
