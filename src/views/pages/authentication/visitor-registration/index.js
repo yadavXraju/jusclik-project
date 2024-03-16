bc441d9ec85d39c1ca4df428d269f27cfbc6b81f
Damandeep <damandeep@shauryasoft>
2024-03-16 13:19:13 +0530

    code modularization and ui refactoring of vistor registration

--- src/views/pages/authentication/visitor-registration/index.js
+++ src/views/pages/authentication/visitor-registration/index.js
@@ -1,15 +1,28 @@
 import React from 'react'
 // import OtpVerification from './otp-verification'
-import { MobileNumber } from './mobile-number/index'
+// import { MobileNumber } from './mobile-number/index'
+import VisitorRegistrationTemplate from './visitor-registration'
+import { Box } from '@mui/material'
 
 const VisitorRegistration = () => {
   return (
     < >
-    
-    <div style={{justifyContent:'center',alignItems:'center',height:'100vh',display:'flex',backgroundColor:'#eef2f6'}}>
-    <MobileNumber/>
+    {/* this will be the static template for or registration process */}
+    {/* parent container box */}
+    <Box sx={{justifyContent:'center',alignItems:'center',height:'100vh',display:'flex',backgroundColor:'#eef2f6'}}>
+    {/* left side logo display */}
+
+
+    {/* right side component */}
+
+    {/* logo when screen size is mobile */}
+
+
+
+    {/* modals render here */}
+    <VisitorRegistrationTemplate/>
     {/* <OtpVerification /> */}
-      </div>     
+      </Box>     
 
 
     </>
