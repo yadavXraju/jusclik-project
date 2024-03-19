a7113ab1eb890c8d7a94dd5f43a35939d955af24
Suraj <suraj@shauryasoft.com>
2024-03-19 17:39:37 +0530

    Inventory Dasahboard Designed

---
+++ src/views/erp-module/inventory-management/inventory-management-dashboard/frequently-issued-items/index.js
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import LinearProgress from '@mui/material/LinearProgress';
+import Typography from '@mui/material/Typography';
+import { Box, Paper, Grid } from '@mui/material';
+
+// Custom component to display linear progress with label
+const LinearProgressWithLabel = ({ value, color }) => {
+  return (
+    <Box width="100%" mr={1}>
+      <LinearProgress variant="determinate" value={value} sx={{ '& .MuiLinearProgress-bar': { backgroundColor: color } }} />
+    </Box>
+  );
+};
+
+const FrequentlyItems = () => {
+  // Array of progress bars with values, labels, and colors
+  const progressBars = [
+    { value: 24, label: 'Sketch Pen', color: "#1b79c3" }, 
+    { value: 20, label: 'Table Calender', color: "#f50057" }, 
+    { value: 60, label: 'Highlighter', color: "#00897b" }, 
+    { value: 85, label: 'Chalk Box', color: "#4caf50" },
+    { value: 18, label: 'Dusters', color: "#9c27b0" } 
+  ];
+
+  return (
+    <Paper variant="elevation" elevation={3}>
+      <Grid
+        sx={{
+          padding: '24px 24px 0 24px',
+          background: '#f9f9fb',
+          borderBottom: '1px solid rgba(128, 128, 128, 0.25)',
+          borderRadius: '12px 12px 0px 0'
+        }}
+      >
+        <Typography variant="h2" sx={{ fontSize: '1.5rem', fontWeight: 'bold', mb: 2 }}>
+          Frequently Issued Items
+        </Typography>
+      </Grid>
+
+      <Box sx={{ width: '100%', padding: '24px' }}>
+        {/* Iterate over the progressBars array and render each progress bar */}
+        {progressBars.map((progress, index) => (
+          <Box key={index} sx={{ marginBottom: 4 }}>
+            <Box sx={{ display: "flex", justifyContent: "space-between", marginBottom: 1 }}>
+              <Typography>{progress.label}</Typography>
+              <Typography>{progress.value} times</Typography>
+            </Box>
+            <LinearProgressWithLabel value={progress.value} color={progress.color} />
+          </Box>
+        ))}
+      </Box>
+    </Paper>
+  );
+};
+
+export default FrequentlyItems;
--- src/views/erp-module/inventory-management/inventory-management-dashboard/index.js
+++ src/views/erp-module/inventory-management/inventory-management-dashboard/index.js
@@ -1,63 +1,52 @@
-
 //inventory module Dashboard.js
 import React from 'react';
 import Grid from '@mui/material/Grid';
 import { gridSpacing } from 'store/constant';
-import CounterData from './CounterData';
-import StockCategory from './StockCategory';
-import StockStatus from './StockStatus'
-import FeeDefaulter from 'views/erp-module/student-information-fee-management/student-info-fee-management-dashboard/dashboard/fee-defaulter';
-import InvoiceRaised from 'views/erp-module/student-information-fee-management/student-info-fee-management-dashboard/dashboard/invoice-raised';
-import FeeCollection from 'views/erp-module/student-information-fee-management/student-info-fee-management-dashboard/dashboard/fee-collection';
+import CounterData from './counter-data/CounterData';
+import StockStatus from './stock-status/StockStatus';
+import StockDetailPieChart from './category-wise-stock/StockDetailPieChart';
+import LowStockItemList from './low-stock-items/LowStockItem';
+import FrequentlyItems from './frequently-issued-items';
 import NeedSupport from 'views/erp-module/student-information-fee-management/student-info-fee-management-dashboard/dashboard/support/Support';
-
 
 const Dashboard = ({ isLoading }) => {
   return (
     <Grid container spacing={gridSpacing}>
       <Grid item xs={12}>
-        <Grid container spacing={gridSpacing} >
-
-         {/* counter */}  
-        <Grid item xs={12} lg={12} md={12} sx={{ marginTop: { md: '0px', lg: '0rem' , display:'flex' , flexDirection:'column', gap:'24px' ,} }}>
-           <CounterData />
-        </Grid>
-
-        {/* Stock Category */}
-        <Grid item xs={12} lg={6} md={12} sx={{ marginTop: { md: '0px', lg: '0rem' } }}>
-                <StockCategory/>
+        <Grid container spacing={gridSpacing}>
+          {/* counter */}
+          <Grid
+            item
+            xs={12}
+            lg={12}
+            md={12}
+            sx={{ marginTop: { md: '0px', lg: '0rem', display: 'flex', flexDirection: 'column', gap: '24px' } }}
+          >
+            <CounterData />
           </Grid>
 
-
-          {/* enrollment stats */}
+          {/* Stock Category */}
+          <Grid item xs={12} lg={6} md={12} sx={{ marginTop: { md: '0px', lg: '0rem' } }}>
+            <StockDetailPieChart />
+          </Grid>
+          {/* Stockt status */}
           <Grid item xs={12} lg={6} md={12} sx={{ marginTop: { md: '0px', lg: '0rem' } }}>
             <StockStatus />
-         </Grid>
-
-
-
-         {/* invoice raised */}
-         <Grid item xs={12} lg={6} md={12} sx={{ marginTop: { md: '0px', lg: '0rem' } }}>
-            <InvoiceRaised />
-         </Grid>
-
-         {/* fee collection */}
-         <Grid item xs={12} lg={6}  md={12} sx={{ marginTop: { md: '0px', lg: '0rem' } }}>
-              <FeeCollection/> 
+          </Grid>
+          {/* Low stock items */}
+          <Grid item xs={12} lg={6} md={12} sx={{ marginTop: { md: '0px', lg: '0rem' } }}>
+            <LowStockItemList />
           </Grid>
 
-           {/* fee defaulter */}
-           <Grid item xs={12} lg={8} md={12} sx={{ marginTop: { md: '0px', lg: '0rem' } }}>
-              <FeeDefaulter isLoading={isLoading} /> 
+          {/* Help & support */}
+          <Grid item xs={12} lg={6} md={12} sx={{ marginTop: { md: '0px', lg: '0rem' } }}>
+            <NeedSupport isLoading={isLoading} />
           </Grid>
-
-          {/* need support */}
-          <Grid item xs={12} lg={4} md={12} sx={{ marginTop: { md: '0px', lg: '0rem' } }}>
-              <NeedSupport isLoading={isLoading} /> 
+          {/* fee collection */}
+          <Grid item xs={12} lg={6} md={12} sx={{ marginTop: { md: '0px', lg: '0rem' } }}>
+            <FrequentlyItems />
           </Grid>
-
         </Grid>
-
       </Grid>
     </Grid>
   );
