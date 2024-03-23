3f05b4b2bd2e68f45714e54478790c2c8ce33006
Kulwinder <kulwinder@shauryasoft>
2024-03-23 14:26:00 +0530

    changes

--- src/views/erp-module/student-information-fee-management/student-info-fee-management-sidebarmenus/settings/setting/invoice-template/kulwinder/template3.js
+++ src/views/erp-module/student-information-fee-management/student-info-fee-management-sidebarmenus/settings/setting/invoice-template/kulwinder/template3.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import Box from '@mui/material/Box';
+import { Grid } from '@mui/material';
+import Paper from '@mui/material/Paper';
 
 function template3() {
   return (
-    <Box sx={{ width: '210mm', height: '297mm', padding: '10px' }}>
+    <Box >
+        <Paper elevation={2} square={true} sx={{ width: '210mm', height: '297mm', padding: '10px' }}>
+
+            <Grid sx={{padding:'10px', border:'1px solid grey'}}>
+               
+
+            </Grid>
+
+          
+
+        </Paper>
+
+       
 
 </Box>
 
