6a036f30a498ee40d30f440419d9883139c551ee
vikash <vikash@shauryasoft.com>
2024-04-03 17:36:12 +0530

    Slab Details

--- src/views/common-section/ParamTime.js
+++ src/views/common-section/ParamTime.js
@@ -3,14 +3,19 @@ import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import dayjs from 'dayjs';
 
 const ParamTime = ({ label="", value=null, onChange,customStyle })=>{
+
+  // ======== Convert value to a valid Date object using dayjs ==========
+  const selectedDate = value ? dayjs(value) : null;
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DemoContainer components={['TimePicker']} sx={customStyle}>
         <TimePicker
           label={label}
-          value={value}
+          value={selectedDate}
           onChange={onChange}
           sx={{width:"100%"}}
         />
