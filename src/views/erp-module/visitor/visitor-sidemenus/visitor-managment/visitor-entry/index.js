4e7e7f0c146ac762e4b175d305c42d8a1c6f4c7e
Harsh Saini <harsh@shauryasoft>
2024-04-03 17:32:08 +0530

    visitor mgmt changes

--- src/views/erp-module/visitor/visitor-sidemenus/visitor-managment/visitor-entry/index.js
+++ src/views/erp-module/visitor/visitor-sidemenus/visitor-managment/visitor-entry/index.js
@@ -4,28 +4,77 @@ import DeleteTwoToneIcon from '@mui/icons-material/DeleteTwoTone';
 // import WarningDialog from 'views/common-section/WarningDialog';
 import EditTwoToneIcon from '@mui/icons-material/EditTwoTone';
 import { IconButton, Tooltip } from '@mui/material';
-import { Box ,Grid, Card, Typography} from '@mui/material';
+import { Box ,Grid, Card, Typography } from '@mui/material';
 import RouteData from './VisitorData';
 import RemoveRedEyeTwoToneIcon from '@mui/icons-material/RemoveRedEyeTwoTone';
+import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined';
+import LoginOutlinedIcon from '@mui/icons-material/LoginOutlined';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
+import Chip from '@mui/material/Chip';
+import Avatar from '@mui/material/Avatar';
 
 
 
+const renderChipCell = (params) => {
+  const { status } = params.row;
+  let iconComponent;
+  switch (status) {
+    case 'Expected':
+      iconComponent = <LoginOutlinedIcon />;
+      break;
+    case 'Checked In':
+      iconComponent = <LogoutOutlinedIcon />;
+      break;
+    case 'Checked Out':
+      iconComponent = <AccessTimeOutlinedIcon/>;
+      break;
+    default:
+      iconComponent = null; // You can set a default icon or handle other cases as needed
+      break;
+  }
+
+
+
+  return (
+    <Chip
+      icon={iconComponent}
+      label= {status}
+      style={{ color: '#1e90ff',width:'140px' }}
+      color="default"
+      variant="outlined"
+      width="200"
+
+    />
+    
+ 
+  );
+};
 
 const columns = [
-  { field: 'VisitorName', headerName: 'Visitor Name', flex: 1, minWidth: 130, align: 'left', headerAlign: 'left' },
+  { field: 'status', headerName: 'Status', flex: 1, minWidth: 150, align: 'left', headerAlign: 'left' ,renderCell: renderChipCell, },
+  { field: 'VisitorName', headerName: 'Visitor Name', flex: 1, minWidth: 160, align: 'left', headerAlign: 'left' , renderCell: (params) => (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <Avatar src={params.row.avatarUrl} alt={params.row.altName} />
+      <span style={{ marginLeft: 8 }}>{params.value}</span>
+    </div>)
+     },
+  { field: 'Address', headerName: 'Address', flex: 1, minWidth: 100 },
+  { field: 'Purpose', headerName: 'Purpose', flex: 1, minWidth: 130 },
+  { field: 'ToMeet', headerName: 'To Meet', flex: 1, minWidth: 130 },
   { field: 'EntryDate', headerName: 'Entry Date', flex: 1, minWidth: 130 },
+  { field: 'GatePass', headerName: 'Gate Pass', flex: 1, minWidth: 130 },
   { 
       field: 'Phone', // Custom field name for combined pick start and end time
       headerName: 'Phone', // Custom header name
       flex: 1,
-      minWidth: 130,
+      minWidth: 100
       
     },
     { 
       field: 'TimeIn', // Custom field name for combined pick start and end time
       headerName: 'Time In', // Custom header name
-      flex: 1,
-      minWidth: 130,
+      // flex: 1,
+      minWidth: 100,
       // valueGetter: (params) => `${params.row.pickstarttime} - ${params.row.pickendtime}`
    
     },
@@ -35,16 +84,12 @@ const columns = [
     minWidth: 100 ,
   //  valueGetter: (params) => `${params.row.dropstarttime} - ${params.row.dropendtime}`,
 },
-  { field: 'Address', headerName: 'Address', flex: 1, minWidth: 100 },
-  { field: 'ToMeet', headerName: 'To Meet', flex: 1, minWidth: 100 },
-  // { field: 'status', headerName: 'Status', flex: 1, minWidth: 100 },
  
-
   {
     field: 'Action',
     headerName: 'Action',
     flex: 1,
-    minWidth: 130,
+    minWidth: 100,
     sortable: false,
     filterable: false,
     disableColumnMenu: true,
@@ -81,15 +126,16 @@ const VisitorEntry = () => {
     <>
       <Card>
         <Box sx={{ borderBottom: '1px solid #ccc' }}>
-          <Grid container spacing={2} p={2} sx={{ alignItems: 'end' }}>
-            <Grid item xs={12} sm={8} lg={8}>
-              <Box sx={{ paddingBottom: '0px' }}>
+          <Grid container spacing={2} p={2} sx={{ alignItems: 'end' ,paddingBottom:'0px' , paddingRight:'0px'}}>
+            <Grid item xs={12} sm={12} lg={12} sx={{}}>
+              <Box sx={{ paddingBottom: '32px' }}>
                 <Typography variant="h4" sx={{ pb: '0px' }}>
                   Visitor List
                 </Typography>
               </Box>
             </Grid>
-          
+       
+
           </Grid>
         </Box>
         <Box p={2}>
@@ -104,10 +150,10 @@ const VisitorEntry = () => {
             }}
             pageSizeOptions={[10, 25, 50, 100]}
             checkboxSelection
-            />
+          />
         </Box>
       </Card>
-     
+
     </>
   )
 }
