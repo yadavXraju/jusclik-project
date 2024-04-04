1a3b8f7d2344c08f73e2316b963e439fb2a5d3d2
Abhishek <abhishek@shauryasoft.com>
2024-04-04 18:17:12 +0530

    changes

--- src/views/erp-module/student-information-fee-management/student-info-fee-management-sidebarmenus/transport/bus-details/index.js
+++ src/views/erp-module/student-information-fee-management/student-info-fee-management-sidebarmenus/transport/bus-details/index.js
@@ -5,7 +5,7 @@ import DeleteTwoToneIcon from '@mui/icons-material/DeleteTwoTone';
 import WarningDialog from 'views/common-section/WarningDialog';
 import EditTwoToneIcon from '@mui/icons-material/EditTwoTone';
 import BusDetailsData from './BusDetailsData';
-import CommonDataGrid from 'views/erp-common-component/commonDataGrid';
+import CommonDataGrid from 'views/common-section/commonDataGrid';
 import BusDetailsDrawer from './BusDetailsDrawer';
 import RemoveRedEyeTwoToneIcon from '@mui/icons-material/RemoveRedEyeTwoTone';
 
