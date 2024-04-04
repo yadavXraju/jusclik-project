c675c1966b89c1feb200ca4bf665d57443c52a7f
Damandeep <damandeep@shauryasoft>
2024-04-04 16:28:37 +0530

    invoice gallery/invoice landing page /redux implement in invoce template

--- src/views/erp-module/student-information-fee-management/student-info-fee-management-sidebarmenus/settings/setting/invoice-template/daman/index.js
+++ src/views/erp-module/student-information-fee-management/student-info-fee-management-sidebarmenus/settings/setting/invoice-template/daman/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import CommonConfig from './common-config-component';
-import { Grid, Paper } from '@mui/material';
+import { Button, Grid, Paper } from '@mui/material';
 import   Template  from '../kulwinder/template3';
-
 import CompactTemplate from '../harsh/template';
 import CompactTemplate2 from '../kulwinder/template2';
 import {CompactTemplate4} from '../harsh/harsh1/template4';
 import CompactTemplate5 from '../harsh/harsh2';
-
-
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router';
 const Index = () => {
-
   // temporary logic for rendring template
-  let template=1
+  const template=useSelector(state=>state.invoiceTemplate.selectedTemplate)
+  const navigate=useNavigate()
   return (
     <>
+    <Button onClick={()=>navigate('/erp/student-info-fee/settings/invoice-template')} sx={{position:'absolute',right:0,marginTop:'-4rem',marginRight:'2rem'}}>Close</Button>
     <Grid container spacing={4}>
-      <Grid item md={5 }>
+      <Grid item md={5}>
       <Paper sx={{width:'100%',height:'auto',padding:' 2rem 0rem'}}>
         <CommonConfig/>
       </Paper>
@@ -26,8 +26,7 @@ const Index = () => {
         {template==2&&<CompactTemplate2/>}
         {template==3&&<Template/>}
         {template==4&&<CompactTemplate4/>}
-        {template==5&&<CompactTemplate5/>}
-              
+        {template==5&&<CompactTemplate5/>}              
       </Grid>
     </Grid>
     </>
