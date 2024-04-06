0da1dfe18c9bbb76afd62cf2ed64a374fce11edd
vikash <vikash@shauryasoft.com>
2024-04-06 14:49:02 +0530

    documents

---
+++ src/views/erp-module/student-information-fee-management/student-info-fee-management-sidebarmenus/transport/bus-details/DocumentData.js
@@ -0,0 +1,77 @@
+import pdf from 'assets/images/pdfIcon.png';
+
+const documentData = [
+    {
+      id: 1,
+      url: 'https://www.thewisdomtree.co/downloads?download=the-jungle-book#54',
+      title: ' Aadhar',
+      subtitle: ' Rudyard Kipling',
+      thumbnailUrl: pdf,
+      lastUpdated: '20-Jan-2029'
+    },
+  
+    {
+      id: 2,
+      url: 'https://www.thewisdomtree.co/downloads?download=the-jungle-book#54',
+      title: ' Fitness',
+      subtitle: ' Rudyard Kipling',
+      thumbnailUrl: pdf,
+      lastUpdated: '20-Jan-2022'
+      //  https://source.unsplash.com/featured?finess
+    },
+  
+    {
+      id: 3,
+      url: 'https://www.thewisdomtree.co/downloads?download=the-jungle-book#54',
+      title: ' License',
+      subtitle: ' Rudyard Kipling',
+      thumbnailUrl: pdf,
+      lastUpdated: '20-Feb-2022'
+    },
+    {
+      id: 4,
+      url: 'https://www.thewisdomtree.co/downloads?download=the-jungle-book#54',
+      title: ' Permit',
+      subtitle: ' Rudyard Kipling',
+      thumbnailUrl: pdf,
+      lastUpdated: '20-Jan-2027'
+    },
+  
+    {
+      id: 5,
+      url: 'https://www.thewisdomtree.co/downloads?download=the-jungle-book#54',
+      title: ' RegCertificate',
+      subtitle: ' Rudyard Kipling',
+      thumbnailUrl: pdf,
+      lastUpdated: '20-July-2021'
+    },
+  
+    {
+      id: 6,
+      url: 'https://www.thewisdomtree.co/downloads?download=the-jungle-book#54',
+      title: ' SpeedGovernor',
+      subtitle: ' Rudyard Kipling',
+      thumbnailUrl: pdf,
+      lastUpdated: '20-Feb-2026'
+    },
+  
+    {
+      id: 7,
+      url: 'https://www.thewisdomtree.co/downloads?download=the-jungle-book#54',
+      title: ' Insurance',
+      subtitle: ' Rudyard Kipling',
+      thumbnailUrl: pdf,
+      lastUpdated: '20-Jan-2023'
+    },
+  
+    {
+      id: 8,
+      url: 'https://www.thewisdomtree.co/downloads?download=the-jungle-book#54',
+      title: ' GPS',
+      subtitle: ' Rudyard Kipling',
+      thumbnailUrl: pdf,
+      lastUpdated: '20-Jan-2025'
+    }
+  ];
+
+  export default documentData;
