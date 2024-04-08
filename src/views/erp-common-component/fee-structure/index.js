c7bffe906fdcd7e1204747029287b0ccd2af17ab
Abhishek <abhishek@shauryasoft.com>
2024-04-08 17:46:04 +0530

    resuable drag and drop table

--- src/views/erp-common-component/fee-structure/index.js
+++ src/views/erp-common-component/fee-structure/index.js
@@ -1,59 +1,73 @@
-import { Box , Typography , Button, Drawer } from '@mui/material';
+import { Box, Typography, Button } from '@mui/material';
 import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
-import  ParamStepper from 'views/common-section/param-stepper';
-import CreateRegularFeeHeads from './regularFeeHead.js/CreateRegularFeeHeads';
+import ParamStepper from 'views/common-section/param-stepper';
+import CreateRegularFeeHeads from './regularFeeHead/CreateRegularFeeHeads';
 import Concession from './concession/Concession';
 import OtherSettings from './other-settings/OtherSettings';
 import useDrawer from 'hooks/useDrawer';
-import NewHead from './regularFeeHead.js/new-head/NewHead';
-import ConcessionDrawer from './concession/ConcessionDrawer';
+// import NewHead from './regularFeeHead/NewHead';
 
 
 
-const tabPage = [
+const FeeStructure = () => {
+  // drawer btn
+  const { anchor, toggleDrawer } = useDrawer();
+  const tabPage = [
     {
       id: 1,
       name: 'Regular Fee Heads',
       value: '1',
       component: CreateRegularFeeHeads,
-      drawerContent :  NewHead ,
+      props: {
+         anchor:anchor,
+         toggleDrawer:toggleDrawer
+      }
     },
     {
-        id: 2,
-        name: 'Optional Fee Heads',
-        value: '2',
-        component:'test',
-        drawerContent :  'test' ,
-      },
+      id: 2,
+      name: 'Optional Fee Heads',
+      value: '2',
+      component: 'test',
+      drawerContetn: 'test',
+      props: {
+        anchor:anchor,
+        toggleDrawer:toggleDrawer
+      }
+    },
   
     {
       id: 3,
       name: 'Concession',
       value: '3',
-      component:Concession,
-      drawerContent :  NewHead ,
+      component: Concession,
+      props: {
+        anchor:anchor,
+        toggleDrawer:toggleDrawer
+      }
     },
   
     {
       id: 4,
       name: 'Discount',
       value: '4',
-      component:CreateRegularFeeHeads,
-      drawerContent :  'NewHead' ,
+      component: CreateRegularFeeHeads,
+      props: {
+        anchor:anchor,
+        toggleDrawer:toggleDrawer
+      }
     },
   
     {
       id: 5,
       name: 'Other Settings',
       value: '5',
-      component:OtherSettings,
-      drawerContent :  '' ,
+      component: OtherSettings,
+      props: {
+        anchor:anchor,
+        toggleDrawer:toggleDrawer
+      }
     }
   ];
-
-const FeeStructure = () => {
-  // drawer btn
-  const  {anchor,toggleDrawer} = useDrawer();
   return (
     <>
       <Box
@@ -72,21 +86,6 @@ const FeeStructure = () => {
           New Head
         </Button>
       </Box>
-
-      {/* Drawer */}
-      <Drawer anchor="right" open={anchor.right} onClose={toggleDrawer('right', false)}>
-        <Box sx={{ width: { xs: '100vw', sm: 650 }, padding: '1rem' }} role="presentation">
-          <Box sx={{ display: 'flex', justifyContent: 'space-between', paddingBottom: '0rem', borderBottom: '1px solid #ccc' }}>
-            <Typography variant="h4"> Fee Structure </Typography>
-            <Button sx={{ marginTop: '-6px' }} onClick={toggleDrawer('right', false)}>
-              Close
-            </Button>
-          </Box>
-
-          {/* <NewHead /> */}
-          <ConcessionDrawer />
-        </Box>
-      </Drawer>
 
       {/* stepper tabs  */}
       <ParamStepper
@@ -107,8 +106,21 @@ const FeeStructure = () => {
         customtabPanelStyle={{ paddingTop: '0', height: 'auto' }}
         customStyleTabs={{ borderRight: '1px solid rgba(224, 224, 224, 1)' }}
       />
+
+      {/* Drawer */}
+      {/* <Drawer anchor="right" open={anchor.right} onClose={toggleDrawer('right', false)}>
+        <Box sx={{ width: { xs: '100vw', sm: 650 }, padding: '1rem' }} role="presentation">
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', paddingBottom: '0rem', borderBottom: '1px solid #ccc' }}>
+            <Typography variant="h4"> Fee Structure </Typography>
+            <Button sx={{ marginTop: '-6px' }} onClick={toggleDrawer('right', false)}>
+              Close
+            </Button>
+          </Box>
+          <NewHead />
+        </Box>
+      </Drawer> */}
     </>
   );
-}
+};
 
-export default FeeStructure
+export default FeeStructure;
