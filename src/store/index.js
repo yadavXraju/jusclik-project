2809ac295123e0cf62fdeb069d017febef7c6161
Abhishek <abhishek@shauryasoft.com>
2024-04-13 17:30:06 +0530

    pricing slice added

--- src/store/index.js
+++ src/store/index.js
@@ -3,15 +3,18 @@ import customizationSlice from "./customization-slice";
 import admissionSlice from './student-info-and-fee/student/admission-slice';
 import InvoiceTemplateSlice from "./student-info-and-fee/setting/Invoice-Template-Slice";
 import userAndRoleAcess from "./student-info-and-fee/user-and-roles/user-and-roles-slice";
+import pricingSlice from "./pages/pricing/pricing-slice";
 
 const store=configureStore({
     reducer:{
         customization:customizationSlice,
         admission:admissionSlice,
         invoiceTemplate:InvoiceTemplateSlice,
-        userRoleAcess:userAndRoleAcess
+        userRoleAcess:userAndRoleAcess,
+        pricingSlice: pricingSlice  ,
     },
     devTools:true
 });
 
 export default store;
+
