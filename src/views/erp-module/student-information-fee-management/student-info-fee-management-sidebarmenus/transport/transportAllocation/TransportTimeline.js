370608627bae11afb3e36ab21453108f8c59885b
vikash <vikash@shauryasoft.com>
2024-04-18 17:53:36 +0530

    Transport Details updated

--- src/views/erp-module/student-information-fee-management/student-info-fee-management-sidebarmenus/transport/transportAllocation/TransportTimeline.js
+++ src/views/erp-module/student-information-fee-management/student-info-fee-management-sidebarmenus/transport/transportAllocation/TransportTimeline.js
@@ -5,11 +5,18 @@ import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
+import { Box } from '@mui/system';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 
 export default function TransportTimeline() {
   return (
+    <>
+    <Box sx={{border:'1px solid #ccc', borderRadius:'5px'}}>
     <Timeline position="alternate">
       <TimelineItem>
+      <TimelineOppositeContent color="text.secondary">
+          09:30 am
+        </TimelineOppositeContent>
         <TimelineSeparator>
           <TimelineDot variant="outlined" />
           <TimelineConnector />
@@ -17,6 +24,9 @@ export default function TransportTimeline() {
         <TimelineContent>Pick</TimelineContent>
       </TimelineItem>
       <TimelineItem>
+      <TimelineOppositeContent color="text.secondary">
+          10:00 am
+        </TimelineOppositeContent>
         <TimelineSeparator>
           <TimelineDot variant="outlined" color="primary" />
           <TimelineConnector />
@@ -24,25 +34,26 @@ export default function TransportTimeline() {
         <TimelineContent>Drop</TimelineContent>
       </TimelineItem>
       <TimelineItem>
+      <TimelineOppositeContent color="text.secondary">
+          12:00 am
+        </TimelineOppositeContent>
         <TimelineSeparator>
           <TimelineDot variant="outlined" color="secondary" />
           <TimelineConnector />
         </TimelineSeparator>
         <TimelineContent>Pick</TimelineContent>
       </TimelineItem>
-      {/* <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot variant="outlined" color="primary" />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Drop</TimelineContent>
-      </TimelineItem> */}
       <TimelineItem>
+      <TimelineOppositeContent color="text.secondary">
+          09:00 am
+        </TimelineOppositeContent>
         <TimelineSeparator>
           <TimelineDot variant="outlined" />
         </TimelineSeparator>
         <TimelineContent>Drop</TimelineContent>
       </TimelineItem>
     </Timeline>
+    </Box>
+    </>
   );
 }
