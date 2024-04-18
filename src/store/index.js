2ef92990795667b2d245dfcfe399d3b4f9cc6d0e
Harsh Saini <harsh@shauryasoft>
2024-04-18 18:05:12 +0530

    commit

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,7 @@ import userAndRoleAcess from "./student-info-and-fee/user-and-roles/user-and-rol
 import pricingSlice from "./pages/pricing/pricing-slice";
 import customForm from './form-builder/custom-form-slice';
 import menuItemSlice from "./layout/mainLayout/sidebar/menuList/navItem/menu-item-slice";
+import configureSlice from './visitor/Visitor-management-slice'
 
 const store=configureStore({
     reducer:{
