6c62e9a99d28a5be0ee99e7689edaaebfe0ea606
vikash <vikash@shauryasoft.com>
2024-04-19 18:00:16 +0530

    preview

--- src/views/erp-module/crm/crm-sidebar/contact/index.js
+++ src/views/erp-module/crm/crm-sidebar/contact/index.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import ContactTable from './LeadTable'
+import { Card, Box, Typography } from '@mui/material'
+import AddContactDrawer from './AddContactDrawer'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+    <Card sx={{padding:'16px'}}>
+    <Box p={1} mb={2} sx={{ borderBottom: '1px solid #ccc', display:'flex', justifyContent:'space-between' }}>
+          <Box sx={{display:'flex', alignItems:'end'}}>
+            <Typography variant="h4" sx={{ pb: '0px' }}>
+              Leads
+            </Typography>
+          </Box>
+          <Box>
+            <AddContactDrawer/>
+          </Box>
+        </Box>
+    <ContactTable/>
+    </Card>
+    </>
   )
 }
 
