f6387036d31ce8ed3ecc587d8f5a3d69f8415755
Damandeep <damandeep@shauryasoft>
2024-04-20 18:02:46 +0530

    making kanban styling

--- src/views/erp-module/crm/crm-sidebar/pipeline/kanban.js
+++ src/views/erp-module/crm/crm-sidebar/pipeline/kanban.js
@@ -3,26 +3,17 @@ import React from 'react';
 import Board from 'react-trello';
 
 const Kanban = ({ data }) => {
-  // const customStyles = {
-  //   boardBackground: 'transparent', // Set the board's background color to transparent
-  //   laneHeaderBackground: '#ffffff', // Example: Set lane header background color to white
-  //   cardBackground: '#ffffff', // Example: Set card background color to white
-  //   laneBackground: '#f0f0f0', // Example: Set lane background color to a light gray
-  // };
-  const customStyles = {
-    board: {
-      background: 'transparent' // Set the board's background color to transparent
-    }
-  };
+
+
   return (
     <Box
       sx={{
         '& .lcvSVC': {
-          backgroundColor: 'transparent',
-          padding: '20px',
-          height:'auto'
+          // backgroundColor: 'transparent',
+          // padding: '20px',
+          // height:'auto'
         },
-        width:'fit-content',
+       
         '& .YtQpS ':{
           backgroundColor:'transparent'
         },
@@ -30,12 +21,43 @@ const Kanban = ({ data }) => {
           padding:'8px'
         },
         '& .eJNaHL':{
-          marginBottom:'10px'
-        }
+          marginBottom:'10px',
+          borderBottom:'0px'
+        },
+        // hide heading
+       '& .bjDsaI ':{
+        display:'none'
+       },
+       '& .gJtTOa:hover':{
+        backgroundColor:'#fff'
+       },
+       '& .kteVYK':{
+        width:'100%'
+       },
+       
       }}
 
     >
-      <Board data={data} draggable style={customStyles} />
+      <Board data={data} draggable 
+      style={{
+        backgroundColor:'transparent',
+          padding: '20px',
+          height:'auto',
+          width:'100%',}}
+          
+          laneStyle={{
+        // backgroundColor:'#f9f9fb',
+        backgroundColor:'#fff',
+        // minWidth:'30%',
+        borderRadius:'15px',
+          }}
+
+          cardStyle={{
+            maxWidth:'none'
+          }}
+          
+      laneDraggable={false}
+      />
     </Box>
   );
 };
