30096a401b09aec3082152beb10a457a047a34e3
sangeeta <sangeeta@shauryasoft.com>
2024-05-09 12:51:59 +0530

    done

--- src/views/erp-module/crm/crm-sidebar/settings/configure/lead-classification/index.js
+++ src/views/erp-module/crm/crm-sidebar/settings/configure/lead-classification/index.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import Systemdata from 'views/common-section/ParamSearchTabs';
+import { leadClassification , leadSource,leadOwnerConfig} from './TabPages';
 
-const LeadClassification = () => {
+const LeadSystemData = () => {
+
+
+  const tabPage = [
+    {
+      id: 2457694,
+      name: "Lead Classification",
+      component: leadClassification ,
+    },
+    {
+      id: 24544,
+      name: "Lead Source",
+      component: leadSource,
+    },
+     {
+      id: 654687,
+      name: "Lead Owner Configurations",
+      component: leadOwnerConfig,
+    },
+ 
+   
+
+
+  ]
+
+
+
   return (
-    <div>LeadClassification</div>
+    <>
+     <Systemdata tabPage={tabPage} />
+    </>
   )
 }
 
-export default LeadClassification
+export default LeadSystemData
