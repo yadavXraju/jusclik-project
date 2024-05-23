fbf4c929dcd7c8890c277eb5c6e10479f3798f85
Kulwinder <kulwinder@shauryasoft>
2024-05-23 10:12:07 +0530

    Testing

--- src/views/common-section/ParamCounters.js
+++ src/views/common-section/ParamCounters.js
@@ -4,7 +4,7 @@ import { Grid, Box, Typography , useMediaQuery } from '@mui/material';
 
 
 
-const ParamCounters = ( {CounterData,reverseOrderFlag ,headingstyle ,subheadingstyle}  ) => {
+const ParamCounters = ( {CounterData,reverseOrderFlag ,headingstyle ,subheadingstyle ,customboxstyle}  ) => {
     
 const isTab =  useMediaQuery('(max-width: 1280px)');
 const isMobile =  useMediaQuery('(max-width: 575px)');
@@ -14,6 +14,16 @@ const isMobile =  useMediaQuery('(max-width: 575px)');
 // }
 
 const  defaultStyles={
+    boxstyle:{ display: 'flex',
+    gap: isTab ? '0px' : '40px',
+    alignItems: 'center',
+    padding:  isTab ? '1.5rem' :'2rem',         
+    borderRadius: '12px',
+    border: '1px solid rgb(227, 227, 227)',
+    boxShadow: '4px 4px 9px 2px #ddddddc2',
+    background:'#fff',
+    justifyContent:isMobile ? 'flex-start' : null } ,
+
     heading:{fontSize: isTab ? '24px' : '32px', color: '#000'},
     subHeading:{fontSize: isTab ? '14px' : '16px', textTransform: 'uppercase', paddingTop: '7px', fontWeight: '500'}
 }
@@ -23,15 +33,7 @@ const  defaultStyles={
                 <Grid xs={12} sm item key={item.id}  >
                     <Box
                         sx={{
-                            display: 'flex',
-                            gap: isTab ? '0px' : '40px',
-                            alignItems: 'center',
-                            padding:  isTab ? '1.5rem' :'2rem',         
-                            borderRadius: '12px',
-                            border: '1px solid rgb(227, 227, 227)',
-                            boxShadow: '4px 4px 9px 2px #ddddddc2',
-                            background:'#fff',
-                            justifyContent:isMobile ? 'flex-start' : null ,
+                            ...defaultStyles.boxstyle , ...customboxstyle
                         }}
                     >
                         {/* icon */}
