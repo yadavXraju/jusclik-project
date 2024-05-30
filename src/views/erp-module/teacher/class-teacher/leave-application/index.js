b8902ca104efcd08c2db422061411347e8e3a4f6
vikash <vikash@shauryasoft.com>
2024-05-30 17:37:58 +0530

    All Drawer Updated

--- src/views/erp-module/teacher/class-teacher/leave-application/index.js
+++ src/views/erp-module/teacher/class-teacher/leave-application/index.js
@@ -1,65 +1,75 @@
 // ======= Page Owner Vikash =========
 // ======= Leave Application Index =========
-import React from 'react'
-import Grid from '@mui/system/Unstable_Grid/Grid'
-import { Box } from '@mui/system';
+import React from 'react';
+import Grid from '@mui/system/Unstable_Grid/Grid';
+import { Box, Paper, Button } from '@mui/material';
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 import LeaveBarChart from './LeaveapplicationBar';
 import LeaveTabs from './LeaveApplicationFilter';
-import { Paper } from '@mui/material';
 import LeaveBalance from './LeaveBalance';
-
-import withParamDrawer from 'components/temp-withParamDrawer';
+import DrawerLayOut from 'layout/drawer-layout';
+import useDrawer from 'hooks/useDrawer';
 import LeaveApplicationDrawerContent from './LeaveApplicationDrawerContent';
 
-const NewParamDrawer=withParamDrawer(LeaveApplicationDrawerContent);
-
-// leave drawer start ==========
-function LeaveDrawer() {
+const DrawerBody = () => {
   return (
-    <div>
-      <NewParamDrawer buttonLabel='Leave Application' drawerTitle='Request Leave' />
-    </div>
-  )
+    <>
+    <LeaveApplicationDrawerContent/>
+    </>
+  );
 }
 
-// leave drawer end ==========
+const LeaveApplication = () => {
+  const { anchor, toggleDrawer } = useDrawer();
 
-const LeaveApplication = () => (
-  <>
-    <Grid container spacing={2}>
-      <Grid item xs={12} sm={7} lg={7}>
-        <Paper elevation={1}>
-          <LeaveBarChart />
-        </Paper>
-      </Grid>
-
-      <Grid item xs={12} sm={5} lg={5}>
-        <Paper elevation={1}>
-          {/* <LeaveChart /> */}
-          <LeaveBalance/>
-        </Paper>
-      </Grid>
-    </Grid>
-    <Paper elevation={1} sx={{ marginBottom: '16px', marginTop: '16px' }}>
-      <Grid container spacing={2} p={2} sx={{ alignItems: 'end' }}>
-        <Grid item xs={12} sm={8} lg={8} >
-          <div>
-            <h3>Pending Leave Details</h3>
-          </div>
+  return (
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={7} lg={7}>
+          <Paper elevation={1}>
+            <LeaveBarChart />
+          </Paper>
         </Grid>
 
-        <Grid item xs={12} sm={4} lg={4} >
-          <div style={{ display: 'flex', justifyContent: 'end', width: '100%' }}>
-            <LeaveDrawer />
-          </div>
+        <Grid item xs={12} sm={5} lg={5}>
+          <Paper elevation={1}>
+            {/* <LeaveChart /> */}
+            <LeaveBalance />
+          </Paper>
         </Grid>
       </Grid>
-      <Box p={2}>
-        <LeaveTabs />
-      </Box>
-    </Paper>
-    {/* <LeaveForm/> */}
-  </>
-)
+      <Paper elevation={1} sx={{ marginBottom: '16px', marginTop: '16px' }}>
+        <Grid container spacing={2} p={2} sx={{ alignItems: 'end' }}>
+          <Grid item xs={12} sm={8} lg={8}>
+            <div>
+              <h3>Pending Leave Details</h3>
+            </div>
+          </Grid>
 
-export default LeaveApplication
+          <Grid item xs={12} sm={4} lg={4}>
+            <div style={{ display: 'flex', justifyContent: 'end', width: '100%' }}>
+              {/* <LeaveDrawer /> */}
+              <Button onClick={toggleDrawer('right', true)} sx={{ mr: '8px' }} variant="outlined" startIcon={<AddOutlinedIcon />}>
+              Leave Application
+            </Button>
+            </div>
+            <DrawerLayOut 
+              anchor={anchor} 
+              direction={'right'} 
+              toggleDrawer={toggleDrawer}
+              Title={'Request Leave'} 
+              Body={<DrawerBody />} 
+              customStyles={{width: { sm: '100vw', md: 700 }}}
+            />
+          </Grid>
+        </Grid>
+        <Box p={2}>
+          <LeaveTabs />
+        </Box>
+      </Paper>
+      {/* <LeaveForm/> */}
+    </>
+  );
+}
+
+export default LeaveApplication;
